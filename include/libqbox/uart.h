b7c37f9828b4d0f2086c109eaeac861fbb1bcde4
Clement Deschamps <clement.deschamps@greensocs.com>
2020-03-20 17:46:28 +0100

    uart.h: fix event notification

--- include/libqbox/uart.h
+++ include/libqbox/uart.h
@@ -81,67 +81,6 @@ public:
     }
 };
 
-#if 0
-#include <ncurses.h>
-
-class CharBackendNCurses : public CharBackend, public sc_core::sc_module {
-private:
-    AsyncEvent m_event;
-    std::queue<unsigned char> m_queue;
-
-public:
-    SC_HAS_PROCESS(CharBackendNCurses);
-
-    CharBackendNCurses(sc_core::sc_module_name name)
-    {
-        SC_METHOD(rcv);
-        sensitive << m_event;
-        dont_initialize();
-
-        pthread_t async_thread;
-        if (pthread_create(&async_thread, NULL, rcv_thread, this)) {
-            fprintf(stderr, "error creating thread\n");
-        }
-    }
-
-    static void *rcv_thread(void *arg)
-    {
-        CharBackendNCurses *serial = (CharBackendNCurses *)arg;
-
-        initscr();
-        noecho();
-        cbreak();
-        timeout(100);
-
-        for (;;) {
-            int c = getch();
-            if (c >= 0) {
-                serial->m_queue.push(c);
-            }
-            if (!serial->m_queue.empty()) {
-                serial->m_event.notify(sc_core::SC_ZERO_TIME);
-            }
-        }
-    }
-
-    void rcv(void)
-    {
-        unsigned char c;
-        while (!m_queue.empty()) {
-            c = m_queue.front();
-            m_queue.pop();
-            m_receive(m_opaque, &c, 1);
-        }
-    }
-
-    void write(unsigned char c)
-    {
-        putchar(c);
-        fflush(stdout);
-    }
-};
-#endif
-
 #ifndef WIN32
 #include <termios.h>
 #include <signal.h>
@@ -226,7 +165,7 @@ public:
                 serial->m_queue.push(c);
             }
             if (!serial->m_queue.empty()) {
-                serial->m_event.notify(sc_core::SC_ZERO_TIME);
+                serial->m_event.async_notify();
             }
         }
     }
@@ -545,3 +484,64 @@ public:
     }
 #endif
 };
+
+#if 0 /* FIXME */
+#include <ncurses.h>
+
+class CharBackendNCurses : public CharBackend, public sc_core::sc_module {
+private:
+    AsyncEvent m_event;
+    std::queue<unsigned char> m_queue;
+
+public:
+    SC_HAS_PROCESS(CharBackendNCurses);
+
+    CharBackendNCurses(sc_core::sc_module_name name)
+    {
+        SC_METHOD(rcv);
+        sensitive << m_event;
+        dont_initialize();
+
+        pthread_t async_thread;
+        if (pthread_create(&async_thread, NULL, rcv_thread, this)) {
+            fprintf(stderr, "error creating thread\n");
+        }
+    }
+
+    static void *rcv_thread(void *arg)
+    {
+        CharBackendNCurses *serial = (CharBackendNCurses *)arg;
+
+        initscr();
+        noecho();
+        cbreak();
+        timeout(100);
+
+        for (;;) {
+            int c = getch();
+            if (c >= 0) {
+                serial->m_queue.push(c);
+            }
+            if (!serial->m_queue.empty()) {
+                serial->m_event.notify(sc_core::SC_ZERO_TIME);
+            }
+        }
+    }
+
+    void rcv(void)
+    {
+        unsigned char c;
+        while (!m_queue.empty()) {
+            c = m_queue.front();
+            m_queue.pop();
+            m_receive(m_opaque, &c, 1);
+        }
+    }
+
+    void write(unsigned char c)
+    {
+        putchar(c);
+        fflush(stdout);
+    }
+};
+#endif
