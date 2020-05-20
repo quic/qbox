02058c7f39e505df3da9427ddd177e3415e8b835
Damien Hedde <damien.hedde@greensocs.com>
2020-05-20 13:46:40 +0200

    models/memory: add transport_dbg support

--- include/libqbox/models/memory.h
+++ include/libqbox/models/memory.h
@@ -43,7 +43,7 @@ public:
         m_ptr = new uint8_t[m_size]();
 
         socket.register_b_transport(this, &Memory::b_transport);
-
+        socket.register_transport_dbg(this, &Memory::transport_dbg);
         socket.register_get_direct_mem_ptr(this, &Memory::get_direct_mem_ptr);
     }
 
@@ -99,6 +99,39 @@ protected:
         txn.set_response_status(tlm::TLM_OK_RESPONSE);
 
         txn.set_dmi_allowed(true);
+    }
+
+    virtual unsigned int transport_dbg(tlm::tlm_generic_payload& txn)
+    {
+        unsigned int len = txn.get_data_length();
+        unsigned char *ptr = txn.get_data_ptr();
+        sc_dt::uint64 addr = txn.get_address();
+
+        if (txn.get_byte_enable_ptr() != 0 || txn.get_streaming_width() < len) {
+            SC_REPORT_ERROR("Memory", "not supported.\n");
+        }
+
+        if ((addr + len) > m_size) {
+            txn.set_response_status(tlm::TLM_ADDRESS_ERROR_RESPONSE);
+            return 0;
+        }
+
+        switch (txn.get_command()) {
+        case tlm::TLM_READ_COMMAND:
+            memcpy(ptr, &m_ptr[addr], len);
+            break;
+        case tlm::TLM_WRITE_COMMAND:
+            memcpy(&m_ptr[addr], ptr, len);
+            break;
+        default:
+            len = 0;
+            SC_REPORT_ERROR("Memory", "TLM command not supported\n");
+            break;
+        }
+
+        txn.set_response_status(tlm::TLM_OK_RESPONSE);
+
+        return len;
     }
 
     virtual bool get_direct_mem_ptr(tlm::tlm_generic_payload& txn, tlm::tlm_dmi& dmi_data)
