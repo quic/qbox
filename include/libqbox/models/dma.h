2e7a5fc0289368ffd99f2b79fe13c4afacfc9203
Clement Deschamps <clement.deschamps@greensocs.com>
2020-09-09 12:23:59 +0200

    models/dma: Use DMI

--- include/libqbox/models/dma.h
+++ include/libqbox/models/dma.h
@@ -23,10 +23,14 @@ class Dma : public sc_core::sc_module {
 public:
     tlm_utils::simple_initiator_socket<Dma> socket;
 
+    tlm::tlm_dmi dmi_data;
+    bool dmi_valid;
+
     Dma(const sc_core::sc_module_name &name)
         : sc_core::sc_module(name)
         , socket("socket")
     {
+        dmi_valid = false;
     }
 
     ~Dma()
@@ -36,6 +40,21 @@ public:
     void bus_do(uint64_t addr, uint8_t *data, uint32_t size, bool is_write)
     {
         tlm::tlm_generic_payload trans;
+
+        if (dmi_valid && addr >= dmi_data.get_start_address() && (addr + size) < dmi_data.get_end_address()) {
+            if (is_write && dmi_data.is_write_allowed()) {
+                uint64_t off = addr - dmi_data.get_start_address();
+                unsigned char *ptr = dmi_data.get_dmi_ptr();
+                memcpy(ptr + off, data, size);
+                return;
+            }
+            else if (!is_write && dmi_data.is_read_allowed()) {
+                uint64_t off = addr - dmi_data.get_start_address();
+                unsigned char *ptr = dmi_data.get_dmi_ptr();
+                memcpy(data, ptr + off, size);
+                return;
+            }
+        }
 
         trans.set_command(is_write ? tlm::TLM_WRITE_COMMAND : tlm::TLM_READ_COMMAND);
         trans.set_address(addr);
@@ -48,6 +67,10 @@ public:
 
         sc_core::sc_time delay(sc_core::SC_ZERO_TIME);
         socket->b_transport(trans, delay);
+
+        if (trans.is_dmi_allowed()) {
+            dmi_valid = socket->get_direct_mem_ptr(trans, dmi_data);
+        }
     }
 
     void bus_write(uint64_t addr, uint8_t *data, uint32_t size)
