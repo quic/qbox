d85d3118f1d6bc44cc6afee10e3af76702306263
BURTON Mark <mark.burton@greensocs.com>
2021-02-23 15:26:31 +0100

    Initial commit for base components, tests missing

---
+++ include/memory.h
@@ -0,0 +1,147 @@
+/*
+ *  This file is part of libqbox
+ *  Copyright (c) 2020 Clement Deschamps
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#pragma once
+
+#include <systemc>
+#include <tlm>
+#include <tlm_utils/simple_target_socket.h>
+
+class Memory : public sc_core::sc_module {
+private:
+    /** Size of the memory in bytes */
+    uint64_t m_size;
+
+    /** Host memory where the data will be stored */
+    uint8_t *m_ptr;
+
+    void b_transport(tlm::tlm_generic_payload &txn, sc_core::sc_time &delay) {
+        unsigned int len = txn.get_data_length();
+        unsigned char *ptr = txn.get_data_ptr();
+        sc_dt::uint64 addr = txn.get_address();
+
+        if (txn.get_byte_enable_ptr() != 0 || txn.get_streaming_width() < len) {
+            SC_REPORT_ERROR("Memory", "not supported.\n");
+        }
+
+        if ((addr + len) > m_size) {
+            txn.set_response_status(tlm::TLM_ADDRESS_ERROR_RESPONSE);
+            return;
+        }
+
+        switch (txn.get_command()) {
+            case tlm::TLM_READ_COMMAND:
+                memcpy(ptr, &m_ptr[addr], len);
+                break;
+            case tlm::TLM_WRITE_COMMAND:
+                memcpy(&m_ptr[addr], ptr, len);
+                break;
+            default:
+                SC_REPORT_ERROR("Memory", "TLM command not supported\n");
+                break;
+        }
+
+        txn.set_response_status(tlm::TLM_OK_RESPONSE);
+
+        txn.set_dmi_allowed(true);
+    }
+
+    unsigned int transport_dbg(tlm::tlm_generic_payload &txn) {
+        unsigned int len = txn.get_data_length();
+        unsigned char *ptr = txn.get_data_ptr();
+        sc_dt::uint64 addr = txn.get_address();
+
+        if (txn.get_byte_enable_ptr() != 0 || txn.get_streaming_width() < len) {
+            SC_REPORT_ERROR("Memory", "not supported.\n");
+        }
+
+        if ((addr + len) > m_size) {
+            txn.set_response_status(tlm::TLM_ADDRESS_ERROR_RESPONSE);
+            return 0;
+        }
+
+        switch (txn.get_command()) {
+            case tlm::TLM_READ_COMMAND:
+                memcpy(ptr, &m_ptr[addr], len);
+                break;
+            case tlm::TLM_WRITE_COMMAND:
+                memcpy(&m_ptr[addr], ptr, len);
+                break;
+            default:
+                len = 0;
+                SC_REPORT_ERROR("Memory", "TLM command not supported\n");
+                break;
+        }
+
+        txn.set_response_status(tlm::TLM_OK_RESPONSE);
+
+        return len;
+    }
+
+    bool get_direct_mem_ptr(tlm::tlm_generic_payload &txn, tlm::tlm_dmi &dmi_data) {
+        static const sc_core::sc_time LATENCY(10, sc_core::SC_NS);
+
+        dmi_data.allow_read_write();
+        dmi_data.set_dmi_ptr(reinterpret_cast<unsigned char *>(&m_ptr[0]));
+        dmi_data.set_start_address(0);
+        dmi_data.set_end_address(m_size - 1);
+        dmi_data.set_read_latency(LATENCY);
+        dmi_data.set_write_latency(LATENCY);
+
+        return true;
+    }
+
+public:
+    tlm_utils::simple_target_socket<Memory> socket;
+
+    Memory(sc_core::sc_module_name name, uint64_t size)
+            : socket("socket") {
+        m_size = size;
+
+        m_ptr = new uint8_t[m_size]();
+
+        socket.register_b_transport(this, &Memory::b_transport);
+        socket.register_transport_dbg(this, &Memory::transport_dbg);
+        socket.register_get_direct_mem_ptr(this, &Memory::get_direct_mem_ptr);
+    }
+
+    Memory() = delete;
+    Memory(const Memory&) = delete;
+
+    ~Memory() {
+        delete[] m_ptr;
+    }
+
+    uint64_t size() {
+        return m_size;
+    }
+
+    size_t load(std::string filename, uint64_t addr) {
+        std::ifstream fin(filename, std::ios::in | std::ios::binary);
+        if (!fin.good()) {
+            printf("Memory::load(): error file not found (%s)\n", filename.c_str());
+            exit(1);
+        }
+        return fin.readsome((char *) &m_ptr[addr], m_size);
+    }
+
+    void load(const uint8_t *ptr, uint64_t len, uint64_t addr) {
+        memcpy(&m_ptr[addr], ptr, len);
+    }
+};
---
+++ include/router.h
@@ -0,0 +1,188 @@
+/*
+ *  Copyright (C) 2020  GreenSocs
+ *
+ */
+
+#pragma once
+
+#include <systemc>
+#include <tlm.h>
+#include <tlm_utils/simple_target_socket.h>
+#include <inttypes.h>
+
+#include "libgs/utils/report.h"
+
+template<unsigned int BUSWIDTH = 32>
+class Router : sc_core::sc_module {
+private:
+    typedef tlm::tlm_base_target_socket_b<BUSWIDTH,
+            tlm::tlm_fw_transport_if<>,
+            tlm::tlm_bw_transport_if<> > target;
+
+    typedef tlm::tlm_base_initiator_socket_b<BUSWIDTH,
+            tlm::tlm_fw_transport_if<>,
+            tlm::tlm_bw_transport_if<> > initiator;
+
+    struct target_info {
+        size_t index;
+        target *t;
+        sc_dt::uint64 address;
+        sc_dt::uint64 size;
+    };
+
+    struct initiator_info {
+        size_t index;
+        initiator *i;
+    };
+
+    std::vector<tlm_utils::simple_target_socket_tagged<Router> *> target_sockets;
+    std::vector<tlm_utils::simple_initiator_socket_tagged<Router> *> initiator_sockets;
+    std::vector<target_info> targets;
+    std::vector<initiator_info> initiators;
+
+    void b_transport(int id, tlm::tlm_generic_payload &trans, sc_core::sc_time &delay) {
+        sc_dt::uint64 addr = trans.get_address();
+        sc_dt::uint64 target_addr;
+        unsigned int target_nr;
+
+        bool success = decode_address(addr, target_addr, target_nr);
+        if (!success) {
+            const char *cmd = "unknown";
+            switch (trans.get_command()) {
+                case tlm::TLM_IGNORE_COMMAND:
+                    cmd = "ignore";
+                    break;
+                case tlm::TLM_WRITE_COMMAND:
+                    cmd = "write";
+                    break;
+                case tlm::TLM_READ_COMMAND:
+                    cmd = "read";
+                    break;
+            }
+
+            GS_LOG("Warning: '%s' access to unmapped address 0x%" PRIx64 " in '%s' module\n",
+                   cmd, static_cast<uint64_t>(trans.get_address()), name());
+            trans.set_response_status(tlm::TLM_ADDRESS_ERROR_RESPONSE);
+            return;
+        }
+
+        trans.set_address(target_addr);
+
+        (*initiator_sockets[target_nr])->b_transport(trans, delay);
+    }
+
+    unsigned int transport_dbg(int id, tlm::tlm_generic_payload &trans) {
+        sc_dt::uint64 addr = trans.get_address();
+        sc_dt::uint64 target_addr;
+        unsigned int target_nr;
+
+        bool success = decode_address(addr, target_addr, target_nr);
+
+        if (!success) {
+            trans.set_response_status(tlm::TLM_ADDRESS_ERROR_RESPONSE);
+            return 0;
+        }
+
+        trans.set_address(target_addr);
+
+        return (*initiator_sockets[target_nr])->transport_dbg(trans);
+    }
+
+    bool get_direct_mem_ptr(int id, tlm::tlm_generic_payload &trans, tlm::tlm_dmi &dmi_data) {
+        sc_dt::uint64 target_addr;
+        unsigned int target_nr;
+
+        bool success = decode_address(trans.get_address(), target_addr, target_nr);
+        if (!success) {
+            return false;
+        }
+
+        trans.set_address(target_addr);
+
+        bool status = (*initiator_sockets[target_nr])->get_direct_mem_ptr(trans, dmi_data);
+        dmi_data.set_start_address(compose_address(target_nr, dmi_data.get_start_address()));
+        dmi_data.set_end_address(compose_address(target_nr, dmi_data.get_end_address()));
+        return status;
+    }
+
+    void invalidate_direct_mem_ptr(int id, sc_dt::uint64 start, sc_dt::uint64 end) {
+        sc_dt::uint64 bw_start_range = compose_address(id, start);
+        sc_dt::uint64 bw_end_range = compose_address(id, end);
+
+        for (auto *socket : target_sockets) {
+            (*socket)->invalidate_direct_mem_ptr(bw_start_range, bw_end_range);
+        }
+    }
+
+    bool decode_address(sc_dt::uint64 addr, sc_dt::uint64 &addr_out, unsigned int &index_out) {
+        for (unsigned int i = 0; i < targets.size(); i++) {
+            struct target_info &ti = targets.at(i);
+            if (addr >= ti.address && addr < (ti.address + ti.size)) {
+                addr_out = addr - ti.address;
+                index_out = i;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    inline sc_dt::uint64 compose_address(unsigned int target_nr, sc_dt::uint64 address) {
+        return targets[target_nr].address + address;
+    }
+
+protected:
+    virtual void before_end_of_elaboration() {
+        for (size_t i = 0; i < initiators.size(); i++) {
+            tlm_utils::simple_target_socket_tagged<Router> *socket =
+                    new tlm_utils::simple_target_socket_tagged<Router>(sc_core::sc_gen_unique_name("target"));
+            socket->register_b_transport(this, &Router::b_transport, i);
+            socket->register_transport_dbg(this, &Router::transport_dbg, i);
+            socket->register_get_direct_mem_ptr(this, &Router::get_direct_mem_ptr, i);
+            socket->bind(*initiators.at(i).i);
+            target_sockets.push_back(socket);
+        }
+
+        for (size_t i = 0; i < targets.size(); i++) {
+            tlm_utils::simple_initiator_socket_tagged<Router> *socket =
+                    new tlm_utils::simple_initiator_socket_tagged<Router>(sc_core::sc_gen_unique_name("initiator"));
+            socket->register_invalidate_direct_mem_ptr(this, &Router::invalidate_direct_mem_ptr, i);
+            socket->bind(*targets.at(i).t);
+            initiator_sockets.push_back(socket);
+        }
+    }
+
+public:
+    explicit Router(const sc_core::sc_module_name &nm)
+            : sc_core::sc_module(nm) {
+        // nothing to do
+    }
+
+    Router() = delete;
+
+    Router(const Router &) = delete;
+
+    ~Router() {
+        for (const auto &i: initiator_sockets) {
+            delete i;
+        }
+        for (const auto &t: target_sockets) {
+            delete t;
+        }
+    }
+
+    void add_target(target &t, uint64_t address, uint64_t size) {
+        struct target_info ti;
+        ti.index = targets.size();
+        ti.t = &t;
+        ti.address = address;
+        ti.size = size;
+        targets.push_back(ti);
+    }
+
+    virtual void add_initiator(initiator &i) {
+        struct initiator_info ii;
+        ii.index = initiators.size();
+        ii.i = &i;
+        initiators.push_back(ii);
+    }
+};
