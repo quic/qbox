2d5c151ed14753139f64e7468470e63e134b64fd
Luc Michel <luc.michel@greensocs.com>
2021-04-29 11:33:30 +0200

    QemuCpuArmCortexA53: add exclusive load/store support
    This commit adds support to handle the exclusive load/store interaction
    with the SystemC world.
    
    Note that the ARM target QEMU implementation of the exclusive load/store
    instruction is not totally accurate w.r.t the real hardware. Each CPU
    keeps a copy of the memory at exclusive load time (env->exclusive_val)
    along with the address of the exclusive access (env->exclusive_addr).
    This value is compared to the current value of the memory at exclusive
    store time. If the value hasn't changed, the exclusive store is
    considered a success and the new value is written into memory. Otherwise
    a failure is returned (this compare and exchange operation is done
    atomically). No locking mechanism is involved from the point of view of
    the global memory system.
    
    On the SystemC side we can have this global lock mechanism though. We
    have this exclusive access TLM extension we must use in case of an
    exclusive access. The other end will indicate, in case of a store,
    whether it succeeded or not.
    
    This commits implement a trade-off to be functionally correct w.r.t
    exclusive accesses on the SystemC side. First of all if the CPU does a
    exclusive load/store pair on a DMI region, there is no way we can
    guarantee anything. This is why if a region is locked on the SystemC
    side, the corresponding DMI region must be invalidated (this is what the
    GreenSocs ExclusiveMonitor component does).
    
    If DMI is properly invalidated, we have a change to catch the exclusive
    store access (and stores only!). This means that the CPU is still not
    able to lock an exclusive region as we won't catch the exclusive loads.
    This is not a problem since the local QEMU implementation of exclusive
    accesses ensure functional correctness.
    
    We can detect the exclusive stores using the following heuristic: since
    QEMU is doing an atomic compare and exchange on exclusive store, we can
    check that:
       1. The CPU is in an exclusive context.
       2. the env->exclusive_addr (set by the previous exclusive load)
          matches the current load/store address.
    
    For 1., the way tcg_gen_atomic_cmpxchg_ixx translates depends on the
    translation context. If QEMU had a memory pointer for the corresponding
    region, it would translate it to some atomic host instruction. Since we
    invalidated the DMI region for this store, the atomic access is not an
    option and the corresponding translation block is re-translated in a
    exclusive context (i.e. not a parallel one), leading to a regular
    qemu_ld, followed by a qemu_st. This is why we check whether the CPU is
    currently running in an exclusive context (meaning that other CPUs are
    blocked). Note that this does _not_ work if QEMU is running in TCG
    single thread mode as exclusive context is never entered in this case.
    This has to be reworked for this case and is left as future work.
    
    For 2., we simply ensure we are not intercepting another load/store.
    
    Now that we know that we are dealing with a exclusive store, and we know
    that we're going to have a load I/O, followed by a store I/O at the same
    address, we can add the exclusive extension for both. The first one will
    try to lock the exclusive region and the second one will report success
    or failure (keep in mind we are already dealing with the exclusive
    store!).
    
    Finally, we report the failure to QEMU if needed. In this case, we have
    to trick QEMU into thinking that the memory value has changed since the
    exclusive load (even if it's not actually the case). We do so by
    smashing the env->exclusive_val value so that it differs from the
    current memory value. Due to some complications in the way QEMU
    implements the checks env->exclusive_val after the atomic_cmpxchg, we
    have to exit the TB so that it gets executed again with the modified
    env->exclusive_val value.
    
    Signed-off-by: Luc Michel <luc.michel@greensocs.com>

--- include/libqbox/components/cpu/arm/cortex-a53.h
+++ include/libqbox/components/cpu/arm/cortex-a53.h
@@ -25,6 +25,8 @@
 
 #include <libqemu-cxx/target/aarch64.h>
 
+#include <greensocs/gsutils/tlm-extensions/exclusive-access.h>
+
 #include "libqbox/components/cpu/cpu.h"
 #include "libqbox/ports/initiator-signal-socket.h"
 #include "libqbox/ports/target-signal-socket.h"
@@ -46,6 +48,28 @@ protected:
             /* TODO: report warning */
             return 0;
         }
+    }
+
+    void add_exclusive_ext(TlmPayload &pl)
+    {
+        ExclusiveAccessTlmExtension *ext = new ExclusiveAccessTlmExtension;
+        ext->add_hop(m_cpu.get_index());
+        pl.set_extension(ext);
+    }
+
+    static uint64_t extract_data_from_payload(const TlmPayload &pl)
+    {
+        uint8_t *ptr = pl.get_data_ptr() + pl.get_data_length() - 1;
+        uint64_t ret = 0;
+
+        /* QEMU never accesses more than 64 bits at the same time */
+        assert(pl.get_data_length() <= 8);
+
+        while (ptr >= pl.get_data_ptr()) {
+            ret = (ret << 8) | *(ptr--);
+        }
+
+        return ret;
     }
 
 public:
@@ -94,11 +118,11 @@ public:
         m_external_ev |= vfiq_in->default_event();
     }
 
-    void before_end_of_elaboration()
+    void before_end_of_elaboration() override
     {
         QemuCpu::before_end_of_elaboration();
 
-        qemu::CpuAarch64 cpu = qemu::CpuAarch64(get_qemu_dev());
+        qemu::CpuAarch64 cpu(m_cpu);
         cpu.set_aarch64_mode(true);
 
         if (!p_mp_affinity.is_default_value()) {
@@ -113,7 +137,7 @@ public:
         cpu.set_prop_int("rvbar", p_rvbar);
     }
 
-    void end_of_elaboration()
+    void end_of_elaboration() override
     {
         QemuCpu::end_of_elaboration();
 
@@ -126,5 +150,101 @@ public:
         irq_timer_virt_out.init(m_dev, 1);
         irq_timer_hyp_out.init(m_dev, 2);
         irq_timer_sec_out.init(m_dev, 3);
+    }
+
+    void initiator_customize_tlm_payload(TlmPayload &payload) override
+    {
+        uint64_t addr;
+        qemu::CpuAarch64 arm_cpu(m_cpu);
+
+        QemuCpu::initiator_customize_tlm_payload(payload);
+
+        addr = payload.get_address();
+
+        if (!arm_cpu.is_in_exclusive_context()) {
+            return;
+        }
+
+        if (addr != arm_cpu.get_exclusive_addr()) {
+            return;
+        }
+
+        /*
+         * We are in the load/store pair of the cmpxchg of the exclusive store
+         * implementation. Add the exclusive access extension to lock the
+         * memory and check for exclusive store success in
+         * initiator_tidy_tlm_payload.
+         */
+        add_exclusive_ext(payload);
+    }
+
+    void initiator_tidy_tlm_payload(TlmPayload &payload) override
+    {
+        using namespace tlm;
+
+        ExclusiveAccessTlmExtension *ext;
+        qemu::CpuAarch64 arm_cpu(m_cpu);
+
+        QemuCpu::initiator_tidy_tlm_payload(payload);
+
+        payload.get_extension(ext);
+        bool exit_tb = false;
+
+        if (ext == nullptr) {
+            return;
+        }
+
+        if (payload.get_command() == TLM_WRITE_COMMAND) {
+            auto sta = ext->get_exclusive_store_status();
+
+            /* We just executed an exclusive store. Check its status */
+            if (sta == ExclusiveAccessTlmExtension::EXCLUSIVE_STORE_FAILURE) {
+                /*
+                 * To actually make the exclusive store fails, we need to trick
+                 * QEMU into thinking that the value at the store address has
+                 * changed compared to when it did the corresponding ldrex (due
+                 * to the way exclusive loads/stores are implemented in QEMU).
+                 * For this, we simply smash the exclusive_val field of the ARM
+                 * CPU state in case it currently matches with the value in
+                 * memory.
+                 */
+                uint64_t exclusive_val = arm_cpu.get_exclusive_val();
+                uint64_t mem_val = extract_data_from_payload(payload);
+                uint64_t mask = (payload.get_data_length() == 8)
+                    ? -1
+                    : (1 << (8 * payload.get_data_length())) - 1;
+
+                if ((exclusive_val & mask) == mem_val) {
+                    arm_cpu.set_exclusive_val(~exclusive_val);
+
+                    /*
+                     * Exit the execution loop to force QEMU to re-do the
+                     * store. This is necessary because we modify exclusive_val
+                     * in the CPU env. This field is also mapped on a TCG
+                     * global. Even though the qemu_st_ixx TCG opcs are marked
+                     * TCG_OPF_CALL_CLOBBER, TCG does not reload the global
+                     * after the store as I thought it would do. To force this,
+                     * we exit the TB here so that the new exclusive_val value
+                     * will be taken into account on the next execution.
+                     */
+                    exit_tb = true;
+                }
+
+                payload.set_response_status(TLM_OK_RESPONSE);
+            }
+        }
+
+        payload.clear_extension(ext);
+        delete ext;
+
+        if (exit_tb) {
+            /*
+             * FIXME: exiting the CPU loop from here is a bit violent. The
+             * caller won't have a chance to destruct its stack objects. The
+             * object model should be reworked to allow exiting the loop
+             * cleanly.
+             */
+            m_cpu.exit_loop_from_io();
+        }
     }
 };
