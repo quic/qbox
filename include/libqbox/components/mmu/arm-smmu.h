73b1bc846bde4163ad5dfa0d3fbb1b05ede01bd7
Tobias Röhmel <tobias.roehmel@greensocs.com>
2022-02-14 09:46:30 +0100

    Add downstream ports

--- include/libqbox/components/mmu/arm-smmu.h
+++ include/libqbox/components/mmu/arm-smmu.h
@@ -30,7 +30,7 @@
 #include "libqbox/ports/target.h"
 #include "libqbox/ports/target-signal-socket.h"
 
-class QemuArmSmmu : public QemuDevice {
+class QemuArmSmmu : public QemuDevice, public QemuInitiatorIface {
 public:
     cci::cci_param<uint32_t> p_pamax;
     cci::cci_param<uint16_t> p_num_smr;
@@ -44,26 +44,30 @@ public:
     sc_core::sc_vector<QemuTargetSocket<>> upstream_socket;
     sc_core::sc_vector<QemuInitiatorSignalSocket> irq_context;
     QemuInitiatorSignalSocket irq_global;
+    sc_core::sc_vector<QemuInitiatorSocket<>> downstream_socket;
 
 
     QemuArmSmmu(sc_core::sc_module_name nm, QemuInstance &inst)
         : QemuDevice(nm, inst, "arm.mmu-500")
         , p_pamax("pamax", 48, "")
-        , p_num_smr("num-smr", 48, "")
-        , p_num_cb("num-cb", 16, "")
-        , p_num_pages("num-pages", 16, "")
+        , p_num_smr("num_smr", 48, "")
+        , p_num_cb("num_cb", 16, "")
+        , p_num_pages("num_pages", 16, "")
         , p_ato("ato", true, "")
         , p_version("version", 0x21, "")
-        , p_num_tbu("num-tbu", 1, "")
-        , upstream_socket("upstream-socket", p_num_tbu, [this] (const char *n, size_t i) {
+        , p_num_tbu("num_tbu", 1, "")
+        , upstream_socket("upstream_socket", p_num_tbu, [this] (const char *n, size_t i) {
             /* here n is already "cpu_<vector-index>" */
             return new QemuTargetSocket<>(n, m_inst);
         })
         , register_socket("mem", inst)
-        , irq_global("irq-global")
-        , irq_context("irq-context", p_num_cb, [this] (const char *n, size_t i) {
+        , irq_global("irq_global")
+        , irq_context("irq_context", p_num_cb, [this] (const char *n, size_t i) {
             /* here n is already "cpu_<vector-index>" */
             return new QemuInitiatorSignalSocket(n);
+        })
+        , downstream_socket("downstream_socket",p_num_tbu,  [this] (const char *n, size_t i) {
+            return new QemuInitiatorSocket<>(n, *this, m_inst);
         })
     {}
 
@@ -78,6 +82,11 @@ public:
         m_dev.set_prop_bool("ato", p_ato);
         m_dev.set_prop_int("version", p_version);
         m_dev.set_prop_int("num-tbu", p_num_tbu);
+
+        std::string base_string = "mr-";
+        for(uint32_t i = 0; i< p_num_tbu; ++i) {
+            downstream_socket[i].init(m_dev, (base_string + std::to_string(i)).c_str());
+        }
     }
 
     void end_of_elaboration() override
@@ -96,6 +105,24 @@ public:
         for(uint32_t i = 0; i < p_num_cb; ++i) {
             irq_context[i].init_sbd(sbd, i + 1);
         }
+    }
+
+     /* QemuInitiatorIface  */
+    virtual void initiator_customize_tlm_payload(TlmPayload &payload) override
+    {
+    }
+
+    virtual void initiator_tidy_tlm_payload(TlmPayload &payload) override
+    {
+    }
+
+    virtual sc_core::sc_time initiator_get_local_time() override
+    {
+        return sc_core::sc_time_stamp();
+    }
+
+    virtual void initiator_set_local_time(const sc_core::sc_time &t) override
+    {
     }
 };
 
