ed2decea601abb09e41291e66d8cea36856fd402
Brian Cain <bcain@quicinc.com>
2022-02-24 08:56:17 -0800

    Add working makena conf
    Tested w/mifs image matching the signature below
    
    $ sha256sum fw/makena/images/mifs_qdrive.img
    f687d76b4c86d713243f4930ab7ba6099cd482cc113cb8cc6f59c79aaa2228d2  fw/makena/images/mifs_qdrive.img

--- makena_conf.lua
+++ makena_conf.lua
@@ -11,6 +11,7 @@ function top()
  end
 
 print ("Lua config running. . . ");
+
  -- to use the hypervisor:
  --KERNEL64_LOAD_ADDR (0x40200000)
  --DTB_LOAD_ADDR      (0x44000000)
@@ -20,9 +21,7 @@ OFFSET_MIFS_DDR_SPACE  = 0x20000000
 OFFSET_SMEM_DDR_SPACE  = 0x00900000
 DDR_SPACE_SIZE = 8*1024*1024*1024 -- 8 GB - should be enough?
 
-UNLIKELY_TO_BE_USED = INITIAL_DDR_SPACE_14GB + OFFSET_SMEM_DDR_SPACE
---DDR_SPACE_SIZE
-MINI_BOOTLOADER = UNLIKELY_TO_BE_USED
+UNLIKELY_TO_BE_USED = INITIAL_DDR_SPACE_14GB + DDR_SPACE_SIZE
 DTB_LOAD_ADDR = UNLIKELY_TO_BE_USED + 512
 
 APSS_GIC600_GICD_APSS = 0x017a00000
@@ -33,7 +32,7 @@ OFFSET_APSS_ALIAS0_GICR_CTLR = 0x60000
 PCIE_3APCIE_WRAPPER_AXI_G3X4_EDMA_AUTO = 0x40000000
 UART0 = PCIE_3APCIE_WRAPPER_AXI_G3X4_EDMA_AUTO
 
-_KERNEL64_LOAD_ADDR = INITIAL_DDR_SPACE_14GB ;--+ OFFSET_SMEM_DDR_SPACE
+_KERNEL64_LOAD_ADDR = INITIAL_DDR_SPACE_14GB
 dofile (top().."fw/arm64_bootloader.lua")
 
 platform = {
@@ -41,34 +40,33 @@ platform = {
     hexagon_num_cpus = 0;
     quantum_ns = 100000000;
 
-    hexagon_start_addr = INITIAL_DDR_SPACE_14GB + 0x0B500000; -- fixme
+    hexagon_start_addr = 0x0B500000; -- fixme
 
     ArmQemuInstance = { tcg_mode="MULTI", sync_policy = "multithread-unconstrained"};
     HexagonQemuInstance = { tcg_mode="SINGLE", sync_policy = "multithread-unconstrained"};
 
--- cpu_0 = { gdb_port = 1234 };
+-- cpu_0 = { gdb_port = 4321 };
 -- hexagon_0 = { gdb_port = 1234 };
 
+    ram=  {  target_socket = {address=INITIAL_DDR_SPACE_14GB, size=8*1024*1024*1024}};
+    hexagon_ram={target_socket={address=UNLIKELY_TO_BE_USED+0x0, size=0x08000000}};
+    rom=  {  target_socket = {address=UNLIKELY_TO_BE_USED+0xde000000, size=0x400 },read_only=true};
+    gic=  {  dist_iface    = {address=APSS_GIC600_GICD_APSS, size=0x10000 };
+             redist_iface_0= {address=APSS_GIC600_GICD_APSS+OFFSET_APSS_ALIAS0_GICR_CTLR, size=0xf60000}};
 
-    ram=  {  target_socket = {address=INITIAL_DDR_SPACE_14GB, size=DDR_SPACE_SIZE}}; -- updated makena
-    hexagon_ram={target_socket={address=0x0, size=0x08000000}}; -- fixme
-    rom=  {  target_socket = {address=0xde000000, size=0x400 },read_only=true};
-    gic=  {  dist_iface    = {address=APSS_GIC600_GICD_APSS, size=0x10000 }; -- updated makena
-             redist_iface_0= {address=OFFSET_APSS_ALIAS0_GICR_CTLR, size=0xf60000}}; -- updated makena
-
-    -- FIXME omit these for now?
-    l2vic={  mem           = {address=0xfc910000, size=0x1000};
-             fastmem       = {address=0xd81e0000, size=0x10000}};
-    qtimer={ mem           = {address=0xfab20000, size=0x1000};
-             timer0_mem    = {address=0xfc921000, size=0x1000};
-             timer1_mem    = {address=0xfc922000, size=0x1000}};
+    -- FIXME these will need to move to the appropriate location:
+    l2vic={  mem           = {address=UNLIKELY_TO_BE_USED+0xfc910000, size=0x1000};
+             fastmem       = {address=UNLIKELY_TO_BE_USED+0xd81e0000, size=0x10000}};
+    qtimer={ mem           = {address=UNLIKELY_TO_BE_USED+0xfab20000, size=0x1000};
+             timer0_mem    = {address=UNLIKELY_TO_BE_USED+0xfc921000, size=0x1000};
+             timer1_mem    = {address=UNLIKELY_TO_BE_USED+0xfc922000, size=0x1000}};
 
     uart= {  mem           = {address=UART0, size=0x1000}};
 
-    ipcc= {  socket        = {address=0x410000, size=0xfc000}}; -- same makena vs waipio/kailua
+    ipcc= {  socket        = {address=0x410000, size=0xfc000}};
 
     -- FIXME: this cannot be removed, so it's configured with minimal size for now
-    system={ target_socket = {address=0x30000000, size=0x10000000}};
+    system={ target_socket = {address=UNLIKELY_TO_BE_USED+0x30000000, size=0x1}, offset=0};
 
     -- FIXME: this is not used on this platform but its presence is masking some
     -- other defect:
@@ -77,29 +75,30 @@ platform = {
     fallback_memory = { target_socket={address=0x0, size=0x10000000}, dmi_allow=false, verbose=true, load={csv_file=top().."fw/makena/SA8540P_MakenaAU_v2_Registers.csv", offset=0, addr_str="Address", value_str="Reset Value", byte_swap=true}};
     load={
         {bin_file=top().."fw/makena/images/mifs_qdrive.img", address=INITIAL_DDR_SPACE_14GB + OFFSET_MIFS_DDR_SPACE };
---        {data=_bootloader_aarch64, address = 0x30000000};--MINI_BOOTLOADER};
---      {data=_bootloader_aarch64, address = 0};
-
+        {bin_file=top().."fw/makena/images/smem_v3.bin", address=INITIAL_DDR_SPACE_14GB + OFFSET_SMEM_DDR_SPACE };
+        -- Entry point for bl31.elf should be set to INITIAL_DDR_SPACE_14GB:
         {elf_file=top().."fw/makena/images/bl31.elf"};
     }
 };
+
 
 if (platform.arm_num_cpus) then
     for i=0,(platform.arm_num_cpus-1) do
         local cpu = {
             has_el3 = true;
-            has_el3 = true;
+            has_el2 = false;
             psci_conduit = "smc";
             mp_affinity = (math.floor(i / 8) << 8) | (i % 8);
             start_powered_off = true;
         };
         if (i==0) then
-            cpu["rvbar"] = 0x80000000;
+            cpu["rvbar"] = INITIAL_DDR_SPACE_14GB;
             cpu["start_powered_off"] = false;
         end
         platform["cpu_"..tostring(i)]=cpu;
     end
 end
+
 
 if (platform.hexagon_num_cpus) then
     for i=1,(platform.hexagon_num_cpus-1) do
@@ -109,6 +108,8 @@ if (platform.hexagon_num_cpus) then
         platform["hexagon_cpu_"..tostring(i)]=cpu;
     end
 end
+
+
 
 
 platform["cfgTable"] = {
