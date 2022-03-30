911e73fa4c180cc5eff3d229de9619daba1fbf1c
Thomas Marceron <thomas.marceron@greensocs.com>
2022-03-30 15:05:18 +0200

    Update the license of the header file

--- include/libqbox/components/irq-ctrl/arm-gicv3.h
+++ include/libqbox/components/irq-ctrl/arm-gicv3.h
@@ -1,6 +1,6 @@
 /*
  *  This file is part of libqbox
- *  Copyright (c) 2020 Clement Deschamps
+ *  Copyright (c) 2020 GreenSocs
  *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public License
