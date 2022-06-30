6585d0907d368ddd040071edd6d33ba77948d8dc
Mark Burton <mburton@quicinc.com>
2022-06-30 15:25:59 +0200

    Add a new 'adaptive' sync policy to try and better share resources on CPU limited hosts
    Signed-off-by: Mark Burton <mburton@quicinc.com>

--- include/greensocs/libgssync.h
+++ include/greensocs/libgssync.h
@@ -28,6 +28,7 @@
 #include "greensocs/libgssync/qkmultithread.h"
 #include "greensocs/libgssync/qkmulti-quantum.h"
 #include "greensocs/libgssync/qkmulti-rolling.h"
+#include "greensocs/libgssync/qkmulti-adaptive.h"
 #include "greensocs/libgssync/qkmulti-unconstrained.h"
 #include "greensocs/libgssync/realtimelimiter.h"
 #include "greensocs/libgssync/qk_factory.h"
