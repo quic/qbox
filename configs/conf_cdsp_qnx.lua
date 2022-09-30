f27b4b04a0c99854c6c68e1afb7b2611437cf049
Brian Cain <bcain@quicinc.com>
2022-09-30 12:43:04 -0700

    Normalize reference configs
    The reference config files have ram_* segments that are required
    by the vBSP.  And now we've updated the reference configs to
    match the tested configuration.  The no-GPU case is also included
    as a reference for those who want to develop in a system without
    a host GPU or without X available.
    
    Signed-off-by: Brian Cain <bcain@quicinc.com>

--- configs/conf_cdsp_qnx.lua
+++ configs/conf_cdsp_qnx.lua
@@ -1,8 +1,12 @@
 print("  local configuration in " .. top());
 tableMerge(platform, {
     virtioblk_0 = { mem = { address = 0x1c0d0000, size = 0x2000 }, irq = 9,
-        blkdev_str = "file=" .. valid_file(top() .. "system_qdrive_qvp.img") .. ",format=raw,if=none" };
+    blkdev_str = "file=" .. valid_file(top() .. "system_8540_qdrive_qvp.img") .. ",format=raw,if=none,readonly=off" };
+    ram_2 = { target_socket = { address = 0x800000000, size = 0x80000000 } };
+    ram_3 = { target_socket = { address = 0xc0000000, size = 0x340000000 } };
 });
+
+
 
 tableJoin(platform["load"], {
     { bin_file = valid_file(top() .. "bl31.bin"), address = INITIAL_DDR_SPACE_14GB };
@@ -13,3 +17,5 @@ tableJoin(platform["load"], {
     { elf_file = valid_file(top() .. "bootimage_relocflag_withdummyseg_makena.cdsp0.prodQ.pbn") };
     { elf_file = valid_file(top() .. "bootimage_relocflag_withdummyseg_makena.cdsp1.prodQ.pbn") };
 });
+
+platform["with_gpu"] = false;
--- configs/conf_ogl_qnx.lua
+++ configs/conf_ogl_qnx.lua
@@ -1,18 +1,21 @@
 print("  local configuration in " .. top());
 tableMerge(platform, {
     virtioblk_0 = { mem = { address = 0x1c0d0000, size = 0x2000 }, irq = 9,
-        blkdev_str = "file=" .. valid_file(top() .. "system_qdrive_qvp.img") .. ",format=raw,if=none,readonly=on" };
+    blkdev_str = "file=" .. valid_file(top() .. "system_8540_qdrive_qvp.img") .. ",format=raw,if=none,readonly=off" };
     ram_2 = { target_socket = { address = 0x800000000, size = 0x80000000 } };
     ram_3 = { target_socket = { address = 0xc0000000, size = 0x340000000 } };
 });
 
+
+
 tableJoin(platform["load"], {
-    { elf_file = valid_file(top() .. "bl31_CRM48.elf") };
-    { bin_file = valid_file(top() .. "mifs_qdrive_qvp.img"),
-        address = INITIAL_DDR_SPACE_14GB + OFFSET_MIFS_DDR_SPACE };
-    { bin_file = valid_file(top() .. "smem_early_qdrive_16gb.bin"),
-        address = INITIAL_DDR_SPACE_14GB + OFFSET_SMEM_DDR_SPACE };
+    { bin_file = valid_file(top() .. "bl31.bin"), address = INITIAL_DDR_SPACE_14GB };
+    { bin_file = valid_file(top() .. "mifs_qdrive_qvp.img"), address = INITIAL_DDR_SPACE_14GB + OFFSET_MIFS_DDR_SPACE };
+    { bin_file = valid_file(top() .. "smem_makena.bin"), address = INITIAL_DDR_SPACE_14GB + OFFSET_SMEM_DDR_SPACE };
+    { bin_file = valid_file(top() .. "cmd_db_header.bin"), address = 0x0C3F0000 };
+    { bin_file = valid_file(top() .. "cmd_db.bin"), address = 0x80860000 };
+    { elf_file = valid_file(top() .. "bootimage_relocflag_withdummyseg_makena.cdsp0.prodQ.pbn") };
+    { elf_file = valid_file(top() .. "bootimage_relocflag_withdummyseg_makena.cdsp1.prodQ.pbn") };
 });
 
 platform["with_gpu"] = true;
-
