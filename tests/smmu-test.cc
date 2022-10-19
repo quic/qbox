5ac4c732179ac8241c0de050326140880fccaed4
Brian Cain <bcain@quicinc.com>
2022-10-18 21:09:10 -0700

    use make_unique<> to allocate broker

--- tests/smmu-test.cc
+++ tests/smmu-test.cc
@@ -3,10 +3,11 @@
 
 #include "quic/smmu500/smmu500.h"
 #include <greensocs/gsutils/cciutils.h>
+#include <memory>
 
 
 int sc_main(int argc, char* argv[]) {
-    auto m_broker = new gs::ConfigurableBroker(argc, argv);
+    auto m_broker = std::make_unique<gs::ConfigurableBroker>(argc, argv);
     smmu500<> smmu("test_smmu");
     return 0;
 }
