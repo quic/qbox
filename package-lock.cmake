db9d8f58cc48f69970812a92b2134caf2fcda6a5
tmarcero <tmarcero@qti.qualcomm.com>
2022-11-10 18:17:12 +0100

    Update package lock file
    - This change switches libqbox to using SysC Common Practices
    for logging, and fixes a bug related to multiple QEMU block
    devices.
    
    The fix for QTOOL-89063 will address errors with multiple virtio
    block devices that appear like the output below:
    
        ...
        Loading libqemu-system-aarch64.so
        Loading libqemu-system-hexagon.so
        RE Loading qqvp/bin/../lib/libqemu/libqemu-system-hexagon.so
        vp: ../../../../libqemu-src/migration/savevm.c:886:
    vmstate_register_with_alias_id: Assertion `!se->compat ||
    se->instance_id == 0' failed.

--- package-lock.cmake
+++ package-lock.cmake
@@ -4,13 +4,13 @@
 # libqbox
 CPMDeclarePackage(libqbox
   NAME libqbox
-  GIT_TAG v6.5.0
+  GIT_TAG v6.6.0
   GIT_REPOSITORY ${GREENSOCS_GIT}qemu/libqbox.git
 )
 # libgsutils
 CPMDeclarePackage(libgsutils
   NAME libgsutils
-  GIT_TAG v2.5.0
+  GIT_TAG v2.6.0
   GIT_REPOSITORY ${GREENSOCS_GIT}greensocs/libgsutils.git
   GIT_SHALLOW on
 )
@@ -29,14 +29,14 @@ CPMDeclarePackage(googletest
 # libgssync
 CPMDeclarePackage(libgssync
   NAME libgssync
-  GIT_TAG v2.3.0
+  GIT_TAG v2.4.0
   GIT_REPOSITORY ${GREENSOCS_GIT}greensocs/libgssync.git
   GIT_SHALLOW on
 )
 # libqemu-cxx
 CPMDeclarePackage(libqemu-cxx
   NAME libqemu-cxx
-  GIT_TAG v4.2.0
+  GIT_TAG v4.2.1
   GIT_REPOSITORY ${GREENSOCS_GIT}qemu/libqemu-cxx.git
 )
 # libqemu
@@ -49,14 +49,14 @@ CPMDeclarePackage(libqemu
 # base-components
 CPMDeclarePackage(base-components
   NAME base-components
-  GIT_TAG v3.5.0
+  GIT_TAG v4.0.0
   GIT_REPOSITORY ${GREENSOCS_GIT}greensocs/components/base-components.git
 #  GIT_SHALLOW on
 )
 
 CPMDeclarePackage(systemc-uarts
   NAME systemc-uarts
-  GIT_TAG v2.2.0
+  GIT_TAG v2.3.0
   GIT_REPOSITORY ${GREENSOCS_GIT}greensocs/components/systemc-uarts.git
   GIT_SHALLOW on
 )
@@ -64,7 +64,7 @@ CPMDeclarePackage(systemc-uarts
 # extra-components
 CPMDeclarePackage(extra-components
   NAME extra-components
-  GIT_TAG v4.1.0
+  GIT_TAG v4.2.0
   GIT_REPOSITORY ${GREENSOCS_GIT}qemu/extra-components.git
   GIT_SHALLOW on
 )
@@ -73,27 +73,27 @@ CPMDeclarePackage(extra-components
 CPMDeclarePackage(ipcc
   NAME ipcc
   GIT_REPOSITORY ${GREENSOCS_GIT}quic/components/ipcc.git
-  GIT_TAG v1.1.0
+  GIT_TAG v1.2.0
   GIT_SHALLOW on
 )
 # QTB
 CPMDeclarePackage(qtb
   NAME qtb
   GIT_REPOSITORY ${GREENSOCS_GIT}quic/components/qtb.git
-  GIT_TAG v1.1.0
+  GIT_TAG v1.2.0
   GIT_SHALLOW on
 )
 # CSR
 CPMDeclarePackage(csr
   NAME csr
   GIT_REPOSITORY ${GREENSOCS_GIT}quic/components/csr.git
-  GIT_TAG v1.1.1
+  GIT_TAG v1.2.0
   GIT_SHALLOW on
 )
 # smmu
 CPMDeclarePackage(smmu500
   NAME smmu500
   GIT_REPOSITORY ${GREENSOCS_GIT}quic/components/smmu500.git
-  GIT_TAG v0.1.0
+  GIT_TAG v0.2.0
   GIT_SHALLOW on
 )
