addd6a65a10a923ba6bf2e05ac5efd242947a89b
tmarcero <tmarcero@quicinc.com>
2023-03-21 10:55:21 +0100

    Update QQVP repo after the move of the repo

--- quic-vp/package-lock.cmake
+++ quic-vp/package-lock.cmake
@@ -1,102 +1,17 @@
 # CPM Package Lock
 # This file should be committed to version control
 
-# libqbox
-CPMDeclarePackage(libqbox
-  NAME libqbox
-  GIT_TAG v7.3.0
-  GIT_REPOSITORY ${GREENSOCS_GIT}qemu/libqbox.git
+# qbox
+CPMDeclarePackage(qbox
+  NAME qbox
+  GIT_TAG main
+  GIT_REPOSITORY ${GREENSOCS_GIT}qbox.git
 )
-# libgsutils
-CPMDeclarePackage(libgsutils
-  NAME libgsutils
-  GIT_TAG v2.6.4
-  GIT_REPOSITORY ${GREENSOCS_GIT}greensocs/libgsutils.git
-  GIT_SHALLOW on
-)
-# libgssync
-CPMDeclarePackage(libgssync
-  NAME libgssync
-  GIT_TAG v2.5.2
-  GIT_REPOSITORY ${GREENSOCS_GIT}greensocs/libgssync.git
-  GIT_SHALLOW on
-)
-# libqemu-cxx
-CPMDeclarePackage(libqemu-cxx
-  NAME libqemu-cxx
-  GIT_TAG v4.2.3
-  GIT_REPOSITORY ${GREENSOCS_GIT}qemu/libqemu-cxx.git
-)
+
 # libqemu
 CPMDeclarePackage(libqemu
   NAME libqemu
   GIT_TAG libqemu-v7.2-v0.5.2
   GIT_REPOSITORY ${GREENSOCS_GIT}qemu/qemu.git
   GIT_SUBMODULES CMakeLists.txt
-)
-# base-components
-CPMDeclarePackage(base-components
-  NAME base-components
-  GIT_TAG v4.3.1
-  GIT_REPOSITORY ${GREENSOCS_GIT}greensocs/components/base-components.git
-#  GIT_SHALLOW on
-)
-
-CPMDeclarePackage(systemc-uarts
-  NAME systemc-uarts
-  GIT_TAG v2.4.4
-  GIT_REPOSITORY ${GREENSOCS_GIT}greensocs/components/systemc-uarts.git
-  GIT_SHALLOW on
-)
-
-# extra-components
-CPMDeclarePackage(extra-components
-  NAME extra-components
-  GIT_TAG v4.3.0
-  GIT_REPOSITORY ${GREENSOCS_GIT}qemu/extra-components.git
-  GIT_SHALLOW on
-)
-
-# IPCC
-CPMDeclarePackage(ipcc
-  NAME ipcc
-  GIT_REPOSITORY ${GREENSOCS_GIT}quic/components/ipcc.git
-  GIT_TAG v1.2.0
-  GIT_SHALLOW on
-)
-# QTB
-CPMDeclarePackage(qtb
-  NAME qtb
-  GIT_REPOSITORY ${GREENSOCS_GIT}quic/components/qtb.git
-  GIT_TAG v1.2.0
-  GIT_SHALLOW on
-)
-# CSR
-CPMDeclarePackage(csr
-  NAME csr
-  GIT_REPOSITORY ${GREENSOCS_GIT}quic/components/csr.git
-  GIT_TAG v1.2.0
-  GIT_SHALLOW on
-)
-# smmu
-CPMDeclarePackage(smmu500
-  NAME smmu500
-  GIT_REPOSITORY ${GREENSOCS_GIT}quic/components/smmu500.git
-  GIT_TAG v0.2.0
-  GIT_SHALLOW on
-)
-
-# qup
-CPMDeclarePackage(qup
-  NAME qup
-  GIT_REPOSITORY ${GREENSOCS_GIT}quic/components/qup.git
-  GIT_TAG v0.1.2
-  GIT_SHALLOW on
-)
-# SCP
-CPMDeclarePackage(SCP
-  NAME SCP
-  GIT_REPOSITORY ${GREENSOCS_GIT}accellera/systemc-common-practices.git
-  GIT_TAG v0.2.0
-  GIT_SHALLOW on
 )
