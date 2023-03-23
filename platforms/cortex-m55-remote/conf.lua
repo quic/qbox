b35aecf8c191cf6333fe00828894192709127eeb
tmarcero <tmarcero@quicinc.com>
2023-03-23 15:24:40 +0100

    Update the platforms and build them at the top level

--- platforms/cortex-m55-remote/conf.lua
+++ platforms/cortex-m55-remote/conf.lua
@@ -11,14 +11,13 @@ function top()
     end
  end
 
-
 platform = {
     ram = {target_socket = {address=0x0, size=0x20000}, shared_memory=true, 
             load={bin_file=top().."fw/cortex-m55/cortex-m55.bin", offset=0}},
     uart =  { simple_target_socket_0 = {address= 0xc0000000, size=0x1000}, irq=0},
 
     remote = {
-        remote_exec_path=top().."build/remote_cpu",
+        remote_exec_path=top().."../../build/platforms/cortex-m55-remote/remote_cpu",
         -- remote_argv = {
         --    {key = "random_arg1", val = "value of random_arg1"},
         --    {key = "random_arg1", val = "value of random_arg2"}}
