bcc24c4fade4db8a8243cf15e4020ca6f4d77595
Matheus Tavares Bernardino <quic_mathbern@quicinc.com>
2023-05-08 15:30:48 -0300

    package-lock: bump up libqemu version
    This includes a fix for macOS CI.
    
    Signed-off-by: Matheus Tavares Bernardino <quic_mathbern@quicinc.com>

--- package-lock.cmake
+++ package-lock.cmake
@@ -9,7 +9,7 @@
 CPMDeclarePackage(qemu
     NAME libqemu
     GIT_REPOSITORY ${GREENSOCS_GIT}qemu/qemu.git
-    GIT_TAG libqemu-v7.2-v0.5.7
+    GIT_TAG libqemu-v7.2-v0.5.8
     GIT_SUBMODULES CMakeLists.txt
     GIT_SHALLOW on
 )
