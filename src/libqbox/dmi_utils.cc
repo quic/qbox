df0785b8148ac7d4aad64827bd9978521b2b9d55
Mark Burton <mburton@quicinc.com>
2023-05-22 08:24:44 +0200

    Move DMI printing utilities out of .h
    Signed-off-by: Mark Burton <mburton@quicinc.com>

---
+++ src/libqbox/dmi_utils.cc
@@ -0,0 +1,23 @@
+#include "libqbox/dmi-manager.h"
+
+std::ostream& operator<<(std::ostream& os, const QemuInstanceDmiManager::DmiRegion& region)
+{
+    os << "DmiRegion[0x" << std::hex << region.get_key() << "-0x" << region.get_end() << "]";
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const tlm::tlm_dmi& info)
+{
+    auto start = QemuInstanceDmiManager::DmiRegion::key_from_tlm_dmi(info);
+    uint64_t size = (info.get_end_address() - info.get_start_address()) + 1;
+    uint64_t end = start + size - 1;
+    os << "TlmDmi[0x" << std::hex << start << "-0x" << end << "]";
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const QemuInstanceDmiManager::DmiRegionAlias& alias)
+{
+    os << "DmiRegionAlias[0x" << std::hex << alias.get_start() << "-0x" << alias.get_end() << "] at host offset 0x"
+       << uintptr_t(alias.get_dmi_ptr());
+    return os;
+}
