5df6a8a972dc685df7b09b88ab88a0c08fcba62c
Mark Burton <mburton@quicinc.com>
2023-06-07 13:57:45 +0200

    Provide a memory initialization option fr gs::Memory
    Signed-off-by: Mark Burton <mburton@quicinc.com>

--- include/greensocs/base-components/memory.h
+++ include/greensocs/base-components/memory.h
@@ -91,14 +91,9 @@ class Memory : public sc_core::sc_module
 
     public:
         SubBlock(uint64_t address, uint64_t len, Memory& mem)
-            : m_len(len)
-            , m_address(address)
-            , m_mem(mem)
-        {
-        }
+            : m_len(len), m_address(address), m_mem(mem) {}
 
-        SubBlock& access(uint64_t address)
-        {
+        SubBlock& access(uint64_t address) {
             // address is the address of where we want to write/read
             // len is the size of the data
 
@@ -112,23 +107,28 @@ class Memory : public sc_core::sc_module
 
             if (!m_use_sub_blocks) {
                 if (!((std::string)m_mem.p_mapfile).empty()) {
-                    if ((m_ptr = MemoryServices::get().map_file(((std::string)(m_mem.p_mapfile)).c_str(), m_len,
-                                                                m_address)) != nullptr) {
+                    if ((m_ptr = MemoryServices::get().map_file(
+                             ((std::string)(m_mem.p_mapfile)).c_str(), m_len, m_address)) !=
+                        nullptr) {
                         m_mapped = true;
                         return *this;
                     }
                 }
                 if (m_mem.p_shmem) {
                     std::stringstream shmname_stream;
-                    shmname_stream << "/" << getpid() << "-" << std::string(m_mem.name()) << std::to_string(m_address);
+                    shmname_stream << "/" << getpid() << "-" << std::string(m_mem.name())
+                                   << std::to_string(m_address);
                     std::string shmname = shmname_stream.str();
-                    if ((m_ptr = MemoryServices::get().map_mem_create(shmname.c_str(), m_len)) != nullptr) {
+                    if ((m_ptr = MemoryServices::get().map_mem_create(shmname.c_str(), m_len)) !=
+                        nullptr) {
                         m_mapped = true;
                         m_shmemID = ShmemIDExtension(shmname, (uint64_t)m_ptr, m_len);
                         return *this;
                     }
                 }
                 if ((m_ptr = MemoryServices::get().alloc(m_len)) != nullptr) {
+                    if (m_mem.p_init_mem)
+                        memset(m_ptr, m_mem.p_init_mem_val, m_len);
                     return *this;
                 }
 
@@ -146,13 +146,13 @@ class Memory : public sc_core::sc_module
             int i = (address - m_address) / (m_sub_size);
 
             if (!m_sub_blocks[i]) {
-                m_sub_blocks[i] = std::make_unique<SubBlock<N>>((i * m_sub_size) + m_address, m_sub_size, m_mem);
+                m_sub_blocks[i] = std::make_unique<SubBlock<N>>((i * m_sub_size) + m_address,
+                                                                m_sub_size, m_mem);
             }
             return m_sub_blocks[i]->access(address);
         }
 
-        uint64_t read_sub_blocks(uint8_t* data, uint64_t offset, uint64_t len)
-        {
+        uint64_t read_sub_blocks(uint8_t* data, uint64_t offset, uint64_t len) {
             uint64_t block_offset = offset - m_address;
             uint64_t block_len = m_len - block_offset;
             uint64_t remain_len = (len < block_len) ? len : block_len;
@@ -162,8 +162,7 @@ class Memory : public sc_core::sc_module
             return remain_len;
         }
 
-        uint64_t write_sub_blocks(const uint8_t* data, uint64_t offset, uint64_t len)
-        {
+        uint64_t write_sub_blocks(const uint8_t* data, uint64_t offset, uint64_t len) {
             uint64_t block_offset = offset - m_address;
             uint64_t block_len = m_len - block_offset;
             uint64_t remain_len = (len < block_len) ? len : block_len;
@@ -179,18 +178,18 @@ class Memory : public sc_core::sc_module
 
         uint64_t get_address() { return m_address; }
 
-        ShmemIDExtension* get_extension()
-        {
-            if (m_shmemID.empty()) return nullptr;
+        ShmemIDExtension* get_extension() {
+            if (m_shmemID.empty())
+                return nullptr;
             return &m_shmemID;
         }
 
-        ~SubBlock()
-        {
+        ~SubBlock() {
             if (m_mapped) {
                 munmap(m_ptr, m_len);
             } else {
-                if (m_ptr) free(m_ptr);
+                if (m_ptr)
+                    free(m_ptr);
             }
         }
     };
@@ -199,9 +198,9 @@ private:
     std::unique_ptr<Memory<BUSWIDTH>::SubBlock<>> m_sub_block;
 
 protected:
-    virtual bool get_direct_mem_ptr(int id, tlm::tlm_generic_payload& txn, tlm::tlm_dmi& dmi_data)
-    {
-        if (!p_dmi) return false;
+    virtual bool get_direct_mem_ptr(int id, tlm::tlm_generic_payload& txn, tlm::tlm_dmi& dmi_data) {
+        if (!p_dmi)
+            return false;
         sc_dt::uint64 addr = txn.get_address();
 
         if (!m_relative_addresses) {
@@ -249,8 +248,7 @@ protected:
         return true;
     }
 
-    virtual void b_transport(int id, tlm::tlm_generic_payload& txn, sc_core::sc_time& delay)
-    {
+    virtual void b_transport(int id, tlm::tlm_generic_payload& txn, sc_core::sc_time& delay) {
         unsigned int len = txn.get_data_length();
         unsigned char* ptr = txn.get_data_ptr();
         sc_dt::uint64 addr = txn.get_address();
@@ -260,7 +258,8 @@ protected:
         if (txn.get_streaming_width() < len) {
             SCP_FATAL(SCMOD) << "not supported.";
         }
-        if (p_verbose) SCP_WARN(SCMOD) << "b_transport :" << scp::scp_txn_tostring(txn);
+        if (p_verbose)
+            SCP_WARN(SCMOD) << "b_transport :" << scp::scp_txn_tostring(txn);
 
         if (!m_relative_addresses) {
             if (addr < m_address) {
@@ -280,7 +279,8 @@ protected:
                 for (unsigned int i = 0; i < len; i++)
                     if (byt[i % bel] == TLM_BYTE_ENABLED) {
                         if (!read(&(ptr[i]), (addr + i), 1)) {
-                            SCP_FATAL(SCMOD) << "Address + length is out of range of the memory size";
+                            SCP_FATAL(SCMOD)
+                                << "Address + length is out of range of the memory size";
                         }
                     }
             } else {
@@ -298,7 +298,8 @@ protected:
                 for (unsigned int i = 0; i < len; i++)
                     if (byt[i % bel] == TLM_BYTE_ENABLED) {
                         if (!write(&(ptr[i]), (addr + i), 1)) {
-                            SCP_FATAL(SCMOD) << "Address + length is out of range of the memory size";
+                            SCP_FATAL(SCMOD)
+                                << "Address + length is out of range of the memory size";
                         }
                     }
             } else {
@@ -314,11 +315,11 @@ protected:
 
         txn.set_response_status(tlm::TLM_OK_RESPONSE);
 
-        if (p_dmi) txn.set_dmi_allowed(true);
+        if (p_dmi)
+            txn.set_dmi_allowed(true);
     }
 
-    virtual unsigned int transport_dbg(int id, tlm::tlm_generic_payload& txn)
-    {
+    virtual unsigned int transport_dbg(int id, tlm::tlm_generic_payload& txn) {
         unsigned int len = txn.get_data_length();
         sc_core::sc_time delay = sc_core::SC_ZERO_TIME;
         b_transport(id, txn, delay);
@@ -328,21 +329,22 @@ protected:
             return 0;
     }
 
-    void cci_ignore(std::string name)
-    {
+    void cci_ignore(std::string name) {
         auto m_broker = cci::cci_get_broker();
         std::string fullname = std::string(sc_module::name()) + "." + name;
         m_broker.ignore_unconsumed_preset_values(
-            [fullname](const std::pair<std::string, cci::cci_value>& iv) -> bool { return iv.first == fullname; });
+            [fullname](const std::pair<std::string, cci::cci_value>& iv) -> bool {
+                return iv.first == fullname;
+            });
     }
 
-    bool read(uint8_t* data, uint64_t offset, uint64_t len)
-    {
+    bool read(uint8_t* data, uint64_t offset, uint64_t len) {
         // force end of elaboration to ensure we fix the sizes
         // this may happen if another model descides to load data into memory as
         // part of it's initialization during before_end_of_elaboration.
 
-        if (!m_sub_block) before_end_of_elaboration();
+        if (!m_sub_block)
+            before_end_of_elaboration();
 
         uint64_t remain_len = 0;
         uint64_t data_ptr_offset = 0;
@@ -362,9 +364,9 @@ protected:
 
         return true;
     }
-    bool write(const uint8_t* data, uint64_t offset, uint64_t len)
-    {
-        if (!m_sub_block) before_end_of_elaboration();
+    bool write(const uint8_t* data, uint64_t offset, uint64_t len) {
+        if (!m_sub_block)
+            before_end_of_elaboration();
 
         uint64_t remain_len = 0;
         uint64_t data_ptr_offset = 0;
@@ -376,7 +378,8 @@ protected:
         while (len > 0) {
             SubBlock<>& blk = m_sub_block->access(offset + data_ptr_offset);
 
-            remain_len = blk.write_sub_blocks(&data[data_ptr_offset], offset + data_ptr_offset, len);
+            remain_len = blk.write_sub_blocks(&data[data_ptr_offset], offset + data_ptr_offset,
+                                              len);
 
             data_ptr_offset += remain_len;
             len -= remain_len;
@@ -386,7 +389,6 @@ protected:
     }
 
 public:
-
     tlm_utils::multi_passthrough_target_socket<Memory<BUSWIDTH>> socket;
     cci::cci_param<bool> p_rom;
     cci::cci_param<bool> p_dmi;
@@ -396,6 +398,8 @@ public:
     cci::cci_param<uint64_t> p_max_block_size;
     cci::cci_param<uint64_t> p_min_block_size;
     cci::cci_param<bool> p_shmem;
+    cci::cci_param<bool> p_init_mem;
+    cci::cci_param<int> p_init_mem_val; // to match the signature of memset
 
     gs::Loader<> load;
 
@@ -410,18 +414,19 @@ public:
         , p_rom("read_only", false, "Read Only Memory (default false)")
         , p_dmi("dmi_allow", true, "DMI allowed (default true)")
         , p_verbose("verbose", false, "Switch on verbose logging")
-        , p_latency("latency", sc_core::sc_time(10, sc_core::SC_NS), "Latency reported for DMI access")
+        , p_latency("latency", sc_core::sc_time(10, sc_core::SC_NS),
+                    "Latency reported for DMI access")
         , p_mapfile("map_file", "", "(optional) file to map this memory")
         , p_max_block_size("max_block_size", 0x100000000, "Maximum size of the sub bloc")
         , p_min_block_size("min_block_size", sysconf(_SC_PAGE_SIZE), "Minimum size of the sub bloc")
         , p_shmem("shared_memory", false, "Allocate using shared memory")
-        , load("load",
-               [&](const uint8_t* data, uint64_t offset, uint64_t len) -> void {
-                   if (!write(data, offset, len)) {
-                       SCP_WARN(SCMOD) << " Offset : 0x" << std::hex << offset << " of the out of range";
-                   }
-               })
-    {
+        , p_init_mem("init_mem", false, "Initialize allocated memory")
+        , p_init_mem_val("init_mem_val", 0, "Value to initialize memory to")
+        , load("load", [&](const uint8_t* data, uint64_t offset, uint64_t len) -> void {
+            if (!write(data, offset, len)) {
+                SCP_WARN(SCMOD) << " Offset : 0x" << std::hex << offset << " of the out of range";
+            }
+        }) {
         SCP_DEBUG(SCMOD) << "Memory constructor";
         MemoryServices::get().init(); // allow any init required
         auto m_broker = cci::cci_get_broker();
@@ -437,8 +442,9 @@ public:
             if (!m_broker.get_preset_cci_value(std::string(name) + "." + SCP_LOG_LEVEL_PARAM_NAME)
                      .template try_get<int>(level) ||
                 level < (int)(scp::log::WARNING)) {
-                m_broker.set_preset_cci_value((std::string(name) + "." + SCP_LOG_LEVEL_PARAM_NAME).c_str(),
-                                              cci::cci_value(static_cast<int>(scp::log::WARNING)));
+                m_broker.set_preset_cci_value(
+                    (std::string(name) + "." + SCP_LOG_LEVEL_PARAM_NAME).c_str(),
+                    cci::cci_value(static_cast<int>(scp::log::WARNING)));
             }
         }
 
@@ -451,8 +457,7 @@ public:
         socket.register_get_direct_mem_ptr(this, &Memory::get_direct_mem_ptr);
     }
 
-    void before_end_of_elaboration()
-    {
+    void before_end_of_elaboration() {
         if (m_sub_block) {
             return;
         }
@@ -470,7 +475,8 @@ public:
 
         m_relative_addresses = true;
         if (m_broker.has_preset_value(ts_name + ".relative_addresses")) {
-            m_relative_addresses = m_broker.get_preset_cci_value(ts_name + ".relative_addresses").get_bool();
+            m_relative_addresses = m_broker.get_preset_cci_value(ts_name + ".relative_addresses")
+                                       .get_bool();
             m_broker.lock_preset_value(ts_name + ".relative_addresses");
         }
     }
@@ -485,20 +491,22 @@ public:
      *
      * @return the size of the memory of type uint64_t
      */
-    uint64_t size()
-    {
+    uint64_t size() {
         if (!m_size) {
             auto m_broker = cci::cci_get_broker();
             std::string ts_name = std::string(sc_module::name()) + ".target_socket";
             if (m_broker.get_preset_cci_value(ts_name + ".0").is_string()) {
                 // deal with an alias
-                if (m_broker.has_preset_value(ts_name + ".size") && m_broker.get_preset_cci_value(ts_name + ".size").is_number()) {
+                if (m_broker.has_preset_value(ts_name + ".size") &&
+                    m_broker.get_preset_cci_value(ts_name + ".size").is_number()) {
                     SCP_WARN(SCMOD)
-                    ("The configuration alias provided ({}) will be ignored as a valid size is also provided.",
+                    ("The configuration alias provided ({}) will be ignored as a valid size is "
+                     "also provided.",
                      ts_name);
                 } else {
                     ts_name = m_broker.get_preset_cci_value(ts_name + ".0").get_string();
-                    if (ts_name[0] == '&') ts_name = (ts_name.erase(0, 1)) + ".target_socket";
+                    if (ts_name[0] == '&')
+                        ts_name = (ts_name.erase(0, 1)) + ".target_socket";
                 }
             }
             if (!m_broker.has_preset_value(ts_name + ".size")) {
@@ -515,20 +523,22 @@ public:
      *
      * @return the address of the memory of type uint64_t
      */
-    uint64_t base()
-    {
+    uint64_t base() {
         if (!m_address_valid) {
             auto m_broker = cci::cci_get_broker();
             std::string ts_name = std::string(sc_module::name()) + ".target_socket";
             if (m_broker.get_preset_cci_value(ts_name + ".0").is_string()) {
                 // deal with an alias
-                if (m_broker.has_preset_value(ts_name + ".address") && m_broker.get_preset_cci_value(ts_name + ".address").is_number()) {
+                if (m_broker.has_preset_value(ts_name + ".address") &&
+                    m_broker.get_preset_cci_value(ts_name + ".address").is_number()) {
                     SCP_WARN(SCMOD)
-                    ("The configuration alias provided ({}) will be ignored as a valid address is also provided.",
+                    ("The configuration alias provided ({}) will be ignored as a valid address is "
+                     "also provided.",
                      ts_name);
                 } else {
                     ts_name = m_broker.get_preset_cci_value(ts_name + ".0").get_string();
-                    if (ts_name[0] == '&') ts_name = (ts_name.erase(0, 1)) + ".target_socket";
+                    if (ts_name[0] == '&')
+                        ts_name = (ts_name.erase(0, 1)) + ".target_socket";
                 }
             }
             if (!m_broker.has_preset_value(ts_name + ".address")) {
