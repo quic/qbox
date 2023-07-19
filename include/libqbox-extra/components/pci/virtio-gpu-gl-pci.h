b9a7730802f9279e028dd78cd44f785effe5660c
Antonio Caggiano <quic_acaggian@quicinc.com>
2023-07-19 16:04:14 +0200

    Check virgl define in config-host.h
    Include config-host.h generated qemu header, and check virgl resource
    blob define directly.
    
    Getting libqemu and calling an exported function while contructing QEMU
    modules is problematic as it would trigger libqemu initialization, which
    needs to happen only after creating all modules and setting all QEMU's
    init arguments.
    
    Signed-off-by: Antonio Caggiano <quic_acaggian@quicinc.com>

--- include/libqbox-extra/components/pci/virtio-gpu-gl-pci.h
+++ include/libqbox-extra/components/pci/virtio-gpu-gl-pci.h
@@ -22,6 +22,8 @@
 
 #include "virtio-gpu.h"
 
+#include "libqemu/config-host.h"
+
 class QemuVirtioGpuGlPci : public QemuVirtioGpu
 {
 public:
@@ -35,17 +37,17 @@ public:
         // On MacOS use libqbox's QemuDisplay SystemC module.
         m_inst.set_display_arg("sdl,gl=on");
 
-        if (m_inst.get().virgl_has_blob()) {
-            // Create memory object for host visible shared memory only if blob
-            // resources are available in virgl otherwise it is not needed.
-            // Also we can NOT enable it on MacOS as it does not have memfd support.
-            m_inst.add_arg("-object");
-            auto memory_object = "memory-backend-memfd,id=mem1,size=" +
-                                 std::to_string(p_hostmem_mb.get_value()) + "M";
-            m_inst.add_arg(memory_object.c_str());
-            m_inst.add_arg("-machine");
-            m_inst.add_arg("memory-backend=mem1");
-        }
+#ifdef HAVE_VIRGL_RESOURCE_BLOB
+        // Create memory object for host visible shared memory only if blob
+        // resources are available in virgl otherwise it is not needed.
+        // Also we can NOT enable it on MacOS as it does not have memfd support.
+        m_inst.add_arg("-object");
+        auto memory_object = "memory-backend-memfd,id=mem1,size=" +
+                             std::to_string(p_hostmem_mb.get_value()) + "M";
+        m_inst.add_arg(memory_object.c_str());
+        m_inst.add_arg("-machine");
+        m_inst.add_arg("memory-backend=mem1");
+#endif // HAVE_VIRGL_RESOURCE_BLOB
 #endif // NOT __APPLE__
     }
 
@@ -53,13 +55,13 @@ public:
         QemuVirtioGpu::before_end_of_elaboration();
 
 #ifndef __APPLE__
-        if (m_inst.get().virgl_has_blob()) {
-            // Enable blob resources and host visible shared memory only if
-            // available in virgl and we are NOT on MacOS as it does not have
-            // /dev/udmabuf support.
-            get_qemu_dev().set_prop_bool("blob", true);
-            get_qemu_dev().set_prop_int("hostmem", p_hostmem_mb);
-        }
+#ifdef HAVE_VIRGL_RESOURCE_BLOB
+        // Enable blob resources and host visible shared memory only if
+        // available in virgl and we are NOT on MacOS as it does not have
+        // /dev/udmabuf support.
+        get_qemu_dev().set_prop_bool("blob", true);
+        get_qemu_dev().set_prop_int("hostmem", p_hostmem_mb);
+#endif // HAVE_VIRGL_RESOURCE_BLOB
 
         get_qemu_dev().set_prop_bool("context_init", true);
 #endif // NOT __APPLE__
