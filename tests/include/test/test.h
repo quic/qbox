b8cdeb7b78d91cfee6b0b512dfc1361ed31834d8
Mark Burton <mburton@quicinc.com>
2024-01-08 10:40:21 +0100

    decrease logging to speed up tests
    Signed-off-by: Mark Burton <mburton@quicinc.com>

--- tests/include/test/test.h
+++ tests/include/test/test.h
@@ -74,8 +74,8 @@ int run_testbench(int argc, char* argv[])
     scp::init_logging(scp::LogConfig()
                           .fileInfoFrom(sc_core::SC_ERROR)
                           .logAsync(false)
-                          .logLevel(scp::log::DBGTRACE) // set log level to DBGTRACE = TRACEALL
-                          .msgTypeFieldWidth(30));      // make the msg type column a bit tighter
+                          .logLevel(scp::log::INFO) // set log level to DBGTRACE = TRACEALL
+                          .msgTypeFieldWidth(30));  // make the msg type column a bit tighter
     gs::ConfigurableBroker m_broker{};
     cci::cci_originator orig{ "sc_main" };
     auto broker_h = m_broker.create_broker_handle(orig);
