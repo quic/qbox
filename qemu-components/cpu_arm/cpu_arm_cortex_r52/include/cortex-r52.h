6d0e3e760e988e90711c0422fd0c9d02202049d0
tmarcero <tmarcero@quicinc.com>
2024-03-14 09:57:06 +0100

    Update the cortex-R52
    - Add rvbar option specific to arm CPU
    - Add QEMU sockets to be bind with an interrupt controller (GIC)

--- qemu-components/cpu_arm/cpu_arm_cortex_r52/include/cortex-r52.h
+++ qemu-components/cpu_arm/cpu_arm_cortex_r52/include/cortex-r52.h
@@ -17,13 +17,42 @@
 
 #include <arm.h>
 #include <device.h>
+#include <ports/qemu-initiator-signal-socket.h>
 #include <ports/qemu-target-signal-socket.h>
 #include <ports/target.h>
 
 class cpu_arm_cortexR52 : public QemuCpuArm
 {
+protected:
+    int get_psci_conduit_val() const
+    {
+        if (p_psci_conduit.get_value() == "disabled") {
+            return 0;
+        } else if (p_psci_conduit.get_value() == "smc") {
+            return 1;
+        } else if (p_psci_conduit.get_value() == "hvc") {
+            return 2;
+        } else {
+            /* TODO: report warning */
+            return 0;
+        }
+    }
+
 public:
     cci::cci_param<bool> p_start_powered_off;
+    cci::cci_param<uint64_t> p_rvbar;
+    cci::cci_param<bool> p_has_el2;
+    cci::cci_param<std::string> p_psci_conduit;
+
+    QemuTargetSignalSocket irq_in;
+    QemuTargetSignalSocket fiq_in;
+    QemuTargetSignalSocket virq_in;
+    QemuTargetSignalSocket vfiq_in;
+
+    QemuInitiatorSignalSocket irq_timer_phys_out;
+    QemuInitiatorSignalSocket irq_timer_virt_out;
+    QemuInitiatorSignalSocket irq_timer_hyp_out;
+    QemuInitiatorSignalSocket irq_timer_sec_out;
 
     cpu_arm_cortexR52(const sc_core::sc_module_name& name, sc_core::sc_object* o)
         : cpu_arm_cortexR52(name, *(dynamic_cast<QemuInstance*>(o)))
@@ -31,9 +60,24 @@ public:
     }
     cpu_arm_cortexR52(sc_core::sc_module_name name, QemuInstance& inst)
         : QemuCpuArm(name, inst, "cortex-r52-arm")
+        , p_has_el2("has_el2", true, "ARM virtualization extensions")
+        , p_rvbar("rvbar", 0ull, "Reset vector base address register value")
         , p_start_powered_off("start_powered_off", false,
                               "Start and reset the CPU "
                               "in powered-off state")
+        , p_psci_conduit("psci_conduit", "disabled",
+                         "Set the QEMU PSCI conduit: "
+                         "disabled->no conduit, "
+                         "hvc->through hvc call, "
+                         "smc->through smc call")
+        , irq_in("irq_in")
+        , fiq_in("fiq_in")
+        , virq_in("virq_in")
+        , vfiq_in("vfiq_in")
+        , irq_timer_phys_out("irq_timer_phys_out")
+        , irq_timer_virt_out("irq_timer_virt_out")
+        , irq_timer_hyp_out("irq_timer_hyp_out")
+        , irq_timer_sec_out("irq_timer_sec_out")
     {
     }
 
@@ -44,7 +88,23 @@ public:
         qemu::CpuArm cpu(m_dev);
 
         cpu.set_prop_bool("start-powered-off", p_start_powered_off);
-        cpu.set_prop_bool("reset-hivecs", true);
+        cpu.set_prop_int("rvbar", p_rvbar);
+        cpu.set_prop_int("psci-conduit", get_psci_conduit_val());
+    }
+
+    void end_of_elaboration() override
+    {
+        QemuCpuArm::end_of_elaboration();
+
+        irq_in.init(m_dev, 0);
+        fiq_in.init(m_dev, 1);
+        virq_in.init(m_dev, 2);
+        vfiq_in.init(m_dev, 3);
+
+        irq_timer_phys_out.init(m_dev, 0);
+        irq_timer_virt_out.init(m_dev, 1);
+        irq_timer_hyp_out.init(m_dev, 2);
+        irq_timer_sec_out.init(m_dev, 3);
     }
 };
 
