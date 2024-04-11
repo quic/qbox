8e9d0be92d696577acc0a1772f3bcb9895c4ac94
Antonio Caggiano <quic_acaggian@quicinc.com>
2024-04-11 18:46:48 +0200

    gpu: Add OpenCL enabled gpu
    Signed-off-by: Antonio Caggiano <quic_acaggian@quicinc.com>

---
+++ qemu-components/pci/virtio_gpu_cl_pci/include/virtio_gpu_cl_pci.h
@@ -0,0 +1,28 @@
+/*
+ *  This file is part of libqbox
+ *  Copyright (c) 2024 Qualcomm Innovation Center, Inc.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _LIBQBOX_COMPONENTS_VIRTIO_GPU_CL_PCI_H
+#define _LIBQBOX_COMPONENTS_VIRTIO_GPU_CL_PCI_H
+
+#include <module_factory_registery.h>
+
+#include <virtio_gpu.h>
+
+class virtio_gpu_cl_pci : public QemuVirtioGpu
+{
+public:
+    cci::cci_param<uint64_t> p_hostmem_mb;
+
+    virtio_gpu_cl_pci(const sc_core::sc_module_name& name, sc_core::sc_object* o, sc_core::sc_object* t);
+    virtio_gpu_cl_pci(const sc_core::sc_module_name& name, QemuInstance& inst, qemu_gpex* gpex);
+
+    void before_end_of_elaboration() override;
+};
+
+extern "C" void module_register();
+
+#endif // _LIBQBOX_COMPONENTS_VIRTIO_GPU_CL_PCI_H
