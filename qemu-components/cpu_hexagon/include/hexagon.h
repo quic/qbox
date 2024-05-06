aa0bc44531348fa02416c275a808419eb2f774d5
Mahmoud Kamel <quic_mkamel@quicinc.com>
2024-05-06 11:47:46 +0200

    convert qemu_cpu_hexagon constructor args to cci parameters
    - make qemu_cpu_hexagon instances more flexible by converting constructor
    arguments: config_table_addr, l2vic_base_addr, qtimer_base_addr, hexagon_start_addr
    and vp_mode to CCI parameters.
    - convert parent scope CCI parametrs: subsystem_id, thread_count, isdben_trusted,
    isdben_secure, coproc, vtcm_base_addr, vtcm_size_kb and num_coproc_instance to
    single hexagon CPU instance scope.

--- qemu-components/cpu_hexagon/include/hexagon.h
+++ qemu-components/cpu_hexagon/include/hexagon.h
@@ -39,37 +39,38 @@ public:
     } Rev_t;
 
     const std::map<std::string, Rev_t> DSP_REVS = {
-        DSP_REV(v66),
-        DSP_REV(v68),
-        DSP_REV(v69),
-        DSP_REV(v73),
-        DSP_REV(v79),
+        DSP_REV(v66), DSP_REV(v68), DSP_REV(v69), DSP_REV(v73), DSP_REV(v79),
     };
     sc_core::sc_vector<QemuTargetSignalSocket> irq_in;
 
-    qemu_cpu_hexagon(const sc_core::sc_module_name& name, sc_core::sc_object* o, uint32_t cfgbase,
-                     uint32_t l2vic_base_addr, uint32_t qtimer_base_addr, uint32_t exec_start_addr, bool vp_mode = true)
-        : qemu_cpu_hexagon(name, *(dynamic_cast<QemuInstance*>(o)), cfgbase, l2vic_base_addr, qtimer_base_addr,
-                           exec_start_addr, vp_mode)
+    qemu_cpu_hexagon(const sc_core::sc_module_name& name, sc_core::sc_object* o)
+        : qemu_cpu_hexagon(name, *(dynamic_cast<QemuInstance*>(o)))
     {
     }
-    qemu_cpu_hexagon(const sc_core::sc_module_name& name, QemuInstance& inst, uint32_t cfgbase,
-                     uint32_t l2vic_base_addr, uint32_t qtimer_base_addr, uint32_t exec_start_addr, bool vp_mode = true)
+    qemu_cpu_hexagon(const sc_core::sc_module_name& name, QemuInstance& inst)
         : QemuCpu(name, inst, "v67-hexagon")
         , m_broker(cci::cci_get_broker())
         , irq_in("irq_in", 8, [](const char* n, int i) { return new QemuTargetSignalSocket(n); })
-        , m_cfgbase(cfgbase)
-        , m_l2vic_base_addr(l2vic_base_addr)
-        , m_qtimer_base_addr(qtimer_base_addr)
-        , m_exec_start_addr(exec_start_addr)
-        , m_vp_mode(vp_mode)
+        , p_cfgbase("config_table_addr", 0xffffffffULL, "config table address")
+        , p_l2vic_base_addr("l2vic_base_addr", 0xffffffffULL, "l2vic base address")
+        , p_qtimer_base_addr("qtimer_base_addr", 0xffffffffULL, "qtimer base address")
+        , p_exec_start_addr("hexagon_start_addr", 0xffffffffULL, "execution start address")
+        , p_vp_mode("vp_mode", true, "override the vp_mode for testing")
         , p_dsp_arch("dsp_arch", "v68", "DSP arch")
         , p_start_powered_off("start_powered_off", false,
                               "Start and reset the CPU "
                               "in powered-off state")
         , p_sched_limit("sched_limit", true, "use sched limit")
         , p_paranoid("paranoid_commit_state", false, "enable per-packet checks")
-
+        , p_subsystem_id("subsystem_id", 0, "subsystem id")
+        , p_hexagon_num_threads("hexagon_num_threads", 8,
+                                "number of hexagon threads") // THREADS_MAX in ./target/hexagon/cpu.h
+        , p_isdben_trusted("isdben_trusted", true, "isdben trusted")
+        , p_isdben_secure("isdben_secure", true, "isdben secure")
+        , p_coproc("coproc", "", "coproc")
+        , p_vtcm_base_addr("vtcm_base_addr", 0, "vtcm base address")
+        , p_vtcm_size_kb("vtcm_size_kb", 0, "vtcm size in kb")
+        , p_num_coproc_instance("num_coproc_instance", 0, "number of coproc instances")
     /*
      * We have no choice but to attach-suspend here. This is fixable but
      * non-trivial. It means that the SystemC kernel will never starve...
@@ -92,46 +93,25 @@ public:
         if (rev != DSP_REVS.end()) {
             dsp_rev = rev->second;
         }
-        cpu.set_prop_int("config-table-addr", m_cfgbase);
+        cpu.set_prop_int("config-table-addr", p_cfgbase);
         cpu.set_prop_int("dsp-rev", dsp_rev);
-        cpu.set_prop_int("l2vic-base-addr", m_l2vic_base_addr);
-        cpu.set_prop_int("qtimer-base-addr", m_qtimer_base_addr);
-        cpu.set_prop_int("exec-start-addr", m_exec_start_addr);
+        cpu.set_prop_int("l2vic-base-addr", p_l2vic_base_addr);
+        cpu.set_prop_int("qtimer-base-addr", p_qtimer_base_addr);
+        cpu.set_prop_int("exec-start-addr", p_exec_start_addr);
         cpu.set_prop_bool("start-powered-off", p_start_powered_off);
         // in case of additional reset, this value will be loaded for PC
-        cpu.set_prop_int("start-evb", m_exec_start_addr);
+        cpu.set_prop_int("start-evb", p_exec_start_addr);
         cpu.set_prop_bool("sched-limit", p_sched_limit);
-        cpu.set_prop_bool("virtual-platform-mode", m_vp_mode);
+        cpu.set_prop_bool("virtual-platform-mode", p_vp_mode);
         cpu.set_prop_bool("paranoid-commit-state", p_paranoid);
-
-        std::string parent = gs::get_parent_name(name());
-        cpu.set_prop_int("subsystem-id", gs::cci_get<uint32_t>(m_broker, parent + ".subsystem_id"));
-        cpu.set_prop_int("thread-count", gs::cci_get<uint32_t>(m_broker, parent + ".hexagon_num_threads"));
-        cpu.set_prop_bool("isdben-trusted", gs::cci_get<bool>(m_broker, parent + ".isdben_trusted"));
-        cpu.set_prop_bool("isdben-secure", gs::cci_get<bool>(m_broker, parent + ".isdben_secure"));
-        std::string coproc_path;
-        if (!gs::cci_get<std::string>(m_broker, parent + ".coproc", coproc_path)) {
-            coproc_path.clear();
-        }
-        cpu.set_prop_str("coproc", coproc_path.data());
-
-        uint64_t vtcm_base_addr;
-        if (!gs::cci_get<uint64_t>(m_broker, parent + ".vtcm_base_addr", vtcm_base_addr)) {
-            vtcm_base_addr = 0;
-        }
-        cpu.set_prop_int("vtcm-base-addr", vtcm_base_addr);
-
-        uint32_t vtcm_size_kb;
-        if (!gs::cci_get<uint32_t>(m_broker, parent + ".vtcm_size_kb", vtcm_size_kb)) {
-            vtcm_size_kb = 0;
-        }
-        cpu.set_prop_int("vtcm-size-kb", vtcm_size_kb);
-
-        uint32_t coproc_instances;
-        if (!gs::cci_get<uint32_t>(m_broker, parent + ".num_coproc_instance", coproc_instances)) {
-            coproc_instances = 0;
-        }
-        cpu.set_prop_int("num-coproc-instance", coproc_instances);
+        cpu.set_prop_int("subsystem-id", p_subsystem_id);
+        cpu.set_prop_int("thread-count", p_hexagon_num_threads);
+        cpu.set_prop_bool("isdben-trusted", p_isdben_trusted);
+        cpu.set_prop_bool("isdben-secure", p_isdben_secure);
+        cpu.set_prop_str("coproc", p_coproc.get_value().data());
+        cpu.set_prop_int("vtcm-base-addr", p_vtcm_base_addr);
+        cpu.set_prop_int("vtcm-size-kb", p_vtcm_size_kb);
+        cpu.set_prop_int("num-coproc-instance", p_num_coproc_instance);
     }
 
     void end_of_elaboration() override
@@ -143,18 +123,24 @@ public:
         }
     }
 
-protected:
-    uint32_t m_cfgbase;
-    uint32_t m_l2vic_base_addr;
-    uint32_t m_qtimer_base_addr;
-    uint32_t m_exec_start_addr;
-    bool m_vp_mode;
-
 public:
+    cci::cci_param<uint64_t> p_cfgbase;
+    cci::cci_param<uint32_t> p_l2vic_base_addr;
+    cci::cci_param<uint32_t> p_qtimer_base_addr;
+    cci::cci_param<uint32_t> p_exec_start_addr;
+    cci::cci_param<bool> p_vp_mode;
     cci::cci_param<bool> p_start_powered_off;
     cci::cci_param<bool> p_sched_limit;
     cci::cci_param<bool> p_paranoid;
     cci::cci_param<std::string> p_dsp_arch;
+    cci::cci_param<uint32_t> p_subsystem_id;
+    cci::cci_param<uint32_t> p_hexagon_num_threads;
+    cci::cci_param<bool> p_isdben_trusted;
+    cci::cci_param<bool> p_isdben_secure;
+    cci::cci_param<std::string> p_coproc;
+    cci::cci_param<uint64_t> p_vtcm_base_addr;
+    cci::cci_param<uint32_t> p_vtcm_size_kb;
+    cci::cci_param<uint32_t> p_num_coproc_instance;
 };
 
 extern "C" void module_register();
--- qemu-components/cpu_hexagon/src/hexagon.cc
+++ qemu-components/cpu_hexagon/src/hexagon.cc
@@ -1,6 +1,3 @@
 #include <hexagon.h>
 
-void module_register()
-{
-    GSC_MODULE_REGISTER_C(qemu_cpu_hexagon, sc_core::sc_object*, uint32_t, uint32_t, uint32_t, uint32_t, bool);
-}
+void module_register() { GSC_MODULE_REGISTER_C(qemu_cpu_hexagon, sc_core::sc_object*); }
