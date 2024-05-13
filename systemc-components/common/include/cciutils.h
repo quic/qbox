b5351d95b6dd101ad76b6f062ba85510f49d6389
Mark Burton <mburton@quicinc.com>
2024-05-13 11:15:11 +0200

    Add helper function to find all SystemC objects of a given type
    Signed-off-by: Mark Burton <mburton@quicinc.com>

--- systemc-components/common/include/cciutils.h
+++ systemc-components/common/include/cciutils.h
@@ -130,6 +130,33 @@ public:
 sc_core::sc_object* find_sc_obj(sc_core::sc_object* m, std::string name, bool test = false);
 
 /**
+ * @brief Helper function to find a sc_objects of a given type
+ * @param m     current parent object (use null to start from the top)
+ * @tparam T typename being searched for
+ * @return std::vector<sc_core::sc_object*> list of sc_objects found
+ */
+template <typename T>
+std::vector<T*> find_sc_objects(sc_core::sc_object* m = nullptr)
+{
+    std::vector<T*> res;
+    if (m) {
+        T* o = dynamic_cast<T*>(m);
+        if (o) res.push_back(o);
+    }
+    std::vector<sc_core::sc_object*> children;
+    if (m) {
+        children = m->get_child_objects();
+    } else {
+        children = sc_core::sc_get_top_level_objects();
+    }
+    for (auto o : children) {
+        auto c = find_sc_objects<T>(o);
+        res.insert(res.end(), std::make_move_iterator(c.begin()), std::make_move_iterator(c.end()));
+    }
+    return res;
+}
+
+/**
  * @brief return the leaf name of the given systemc hierarchical name
  *
  * @param name
