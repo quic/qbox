38200efb6d8340b825f19d74793ab091470e8a82
Mark Burton <mburton@quicinc.com>
2024-09-26 13:01:48 +0200

    Add a exiter module - access it, and exit is called with the value in the txn
    Signed-off-by: Mark Burton <mburton@quicinc.com>
    Signed-off-by: Marco Liebel <quic_mliebel@quicinc.com>

---
+++ systemc-components/exiter/src/exiter.cc
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <exiter.h>
+
+void module_register() { GSC_MODULE_REGISTER_C(exiter); }
