ba581ea3c2e666b98887ed3ff9729dfc953d6a82
Mahmoud Kamel <quic_mkamel@quicinc.com>
2024-12-02 18:07:16 +0100

    package-lock: use tagged SC libraries
    - specific SystemCCCI, SystemCLanguage and SCP git tags should be
    used to mitigate the possible unstability issues on main branches
    pulled by default.
    
    Signed-off-by: Mahmoud Kamel <quic_mkamel@quicinc.com>

--- package-lock.cmake
+++ package-lock.cmake
@@ -5,6 +5,35 @@
 # is _not_ a submodule so that FetchContent does not init any submodule.
 # See https://gitlab.kitware.com/cmake/cmake/-/issues/20579
 
+# cci
+CPMDeclarePackage(SystemCCCI
+  NAME SystemCCCI
+  GIT_TAG v1.0.1
+  GIT_REPOSITORY https://github.com/accellera-official/cci.git
+  GIT_SHALLOW on
+  OPTIONS
+      "SYSTEMCCCI_BUILD_TESTS OFF"
+)
+
+# SystemC
+CPMDeclarePackage(SystemCLanguage
+  NAME SystemCLanguage
+  GIT_TAG 3.0.0
+  GIT_REPOSITORY https://github.com/accellera-official/systemc.git
+  GIT_SHALLOW on
+  OPTIONS
+      "ENABLE_SUSPEND_ALL"
+      "ENABLE_PHASE_CALLBACKS"
+)
+
+# SCP
+CPMDeclarePackage(SCP
+  NAME SCP
+  GIT_TAG v0.1.0
+  GIT_REPOSITORY https://github.com/accellera-official/systemc-common-practices.git
+  GIT_SHALLOW on
+)
+
 # qemu (unversioned)
 CPMDeclarePackage(qemu
     NAME libqemu
