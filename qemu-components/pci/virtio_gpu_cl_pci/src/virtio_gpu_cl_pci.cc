4c11306986746b5d8c31f63c6142a193903e10a4
Marco Liebel <mliebel@quicinc.com>
2024-12-13 12:03:23 -0800

    Use VCL-specific defines for virtio_gpu_cl_pci
    Signed-off-by: Marco Liebel <mliebel@quicinc.com>

--- qemu-components/pci/virtio_gpu_cl_pci/src/virtio_gpu_cl_pci.cc
+++ qemu-components/pci/virtio_gpu_cl_pci/src/virtio_gpu_cl_pci.cc
@@ -21,7 +21,7 @@ virtio_gpu_cl_pci::virtio_gpu_cl_pci(const sc_core::sc_module_name& name, QemuIn
     , p_hostmem_mb("hostmem_mb", 2048, "MB to allocate for host visible shared memory")
 {
 #ifndef __APPLE__
-#ifdef HAVE_VIRGL_RESOURCE_BLOB
+#ifdef HAVE_VCL_RESOURCE_BLOB
     // Create memory object for host visible shared memory only if blob
     // resources are available in virgl otherwise it is not needed.
     // Also we can NOT enable it on MacOS as it does not have memfd support.
@@ -30,7 +30,7 @@ virtio_gpu_cl_pci::virtio_gpu_cl_pci(const sc_core::sc_module_name& name, QemuIn
     m_inst.add_arg(memory_object.c_str());
     m_inst.add_arg("-machine");
     m_inst.add_arg("memory-backend=mem1");
-#endif // HAVE_VIRGL_RESOURCE_BLOB
+#endif // HAVE_VCL_RESOURCE_BLOB
 #endif // NOT __APPLE__
 }
 
@@ -39,13 +39,13 @@ void virtio_gpu_cl_pci::before_end_of_elaboration()
     QemuVirtioGpu::before_end_of_elaboration();
 
 #ifndef __APPLE__
-#ifdef HAVE_VIRGL_RESOURCE_BLOB
+#ifdef HAVE_VCL_RESOURCE_BLOB
     // Enable blob resources and host visible shared memory only if
     // available in virgl and we are NOT on MacOS as it does not have
     // /dev/udmabuf support.
     get_qemu_dev().set_prop_bool("blob", true);
     get_qemu_dev().set_prop_int("hostmem", p_hostmem_mb);
-#endif // HAVE_VIRGL_RESOURCE_BLOB
+#endif // HAVE_VCL_RESOURCE_BLOB
 
     get_qemu_dev().set_prop_bool("context_init", true);
 #endif // NOT __APPLE__
