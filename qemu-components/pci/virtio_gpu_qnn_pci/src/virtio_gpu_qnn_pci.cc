e047f00f8b323d485098713d1b3d826763c2d32b
Nikhita Nadgauda <quic_nnadgaud@quicinc.com>
2025-01-13 15:07:54 -0800

    Add support for virqnnrenderer
    Change-Id: Iea15c6acff2b07346bea118e25316cb46d6d0aa3
    Signed-off-by: Nikhita Nadgauda <quic_nnadgaud@quicinc.com>

---
+++ qemu-components/pci/virtio_gpu_qnn_pci/src/virtio_gpu_qnn_pci.cc
@@ -0,0 +1,52 @@
+/*
+ *  This file is part of libqbox
+ *  Copyright (c) 2024-2025 Qualcomm Innovation Center, Inc.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <virtio_gpu_qnn_pci.h>
+
+#include <libqemu/config-host.h>
+
+virtio_gpu_qnn_pci::virtio_gpu_qnn_pci(const sc_core::sc_module_name& name, sc_core::sc_object* o,
+                                       sc_core::sc_object* t)
+    : virtio_gpu_qnn_pci(name, *(dynamic_cast<QemuInstance*>(o)), (dynamic_cast<qemu_gpex*>(t)))
+{
+}
+virtio_gpu_qnn_pci::virtio_gpu_qnn_pci(const sc_core::sc_module_name& name, QemuInstance& inst, qemu_gpex* gpex)
+    : QemuVirtioGpu(name, inst, "qnn-pci", gpex)
+    , p_hostmem_mb("hostmem_mb", 2048, "MB to allocate for host visible shared memory")
+{
+#ifndef __APPLE__
+#ifdef HAVE_VQNN_RESOURCE_BLOB
+    // Create memory object for host visible shared memory only if blob
+    // resources are available in virgl otherwise it is not needed.
+    // Also we can NOT enable it on MacOS as it does not have memfd support.
+    m_inst.add_arg("-object");
+    auto memory_object = "memory-backend-memfd,id=mem1,size=" + std::to_string(p_hostmem_mb.get_value()) + "M";
+    m_inst.add_arg(memory_object.c_str());
+    m_inst.add_arg("-machine");
+    m_inst.add_arg("memory-backend=mem1");
+#endif // HAVE_VQNN_RESOURCE_BLOB
+#endif // NOT __APPLE__
+}
+
+void virtio_gpu_qnn_pci::before_end_of_elaboration()
+{
+    QemuVirtioGpu::before_end_of_elaboration();
+
+#ifndef __APPLE__
+#ifdef HAVE_VQNN_RESOURCE_BLOB
+    // Enable blob resources and host visible shared memory only if
+    // available in virgl and we are NOT on MacOS as it does not have
+    // /dev/udmabuf support.
+    get_qemu_dev().set_prop_bool("blob", true);
+    get_qemu_dev().set_prop_int("hostmem", p_hostmem_mb);
+#endif // HAVE_VQNN_RESOURCE_BLOB
+
+    get_qemu_dev().set_prop_bool("context_init", true);
+#endif // NOT __APPLE__
+}
+
+void module_register() { GSC_MODULE_REGISTER_C(virtio_gpu_qnn_pci, sc_core::sc_object*, sc_core::sc_object*); }
