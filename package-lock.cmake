3ec77ce96fcf62ef848a298f9a68bce7937583a7
Matheus Tavares Bernardino <matheus.bernardino@oss.qualcomm.com>
2025-12-16 07:24:27 -0800

    package-lock: update libqemu to libqemu-v10.1-v0.5
    This brings in the following libqemu commits:
      - 7717b6ee (kvm: add missing error arg to kvm_arch_get_registers, 2025-12-16)
    
    Signed-off-by: Matheus Tavares Bernardino <matheus.bernardino@oss.qualcomm.com>

--- package-lock.cmake
+++ package-lock.cmake
@@ -27,7 +27,7 @@ CPMDeclarePackage(SCP
 CPMDeclarePackage(qemu
     NAME libqemu
     GIT_REPOSITORY ${GREENSOCS_GIT}${QEMU_PATH_NAME}.git
-    GIT_TAG libqemu-v10.1-v0.4
+    GIT_TAG libqemu-v10.1-v0.5
     GIT_SUBMODULES CMakeLists.txt
     GIT_SHALLOW on
 )
