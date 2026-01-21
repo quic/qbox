a413bb150d717c6d8cab4d64bd69c91b7dca5c80
Mahmoud Kamel <quic_mkamel@quicinc.com>
2026-01-21 13:15:39 +0100

    qemu-components: remove the hard dependency on QemuVirtioGpu from the display constructor
    - To use the display device under qemu-components with other types of graphics
    devices i.e. ramfb, the dependency on the QemuVirtioGpu* on the constructor
    should be removed and only used if vnc is enabled.
    
    Signed-off-by: Mahmoud Kamel <quic_mkamel@quicinc.com>

--- qemu-components/display/include/display.h
+++ qemu-components/display/include/display.h
@@ -226,6 +226,8 @@ private:
 
 class display : public sc_core::sc_module
 {
+    SCP_LOGGER();
+
 protected:
     sc_core::sc_vector<VncConfiguration> vnc;
 
@@ -234,7 +236,7 @@ private:
     MainThreadQemuDisplay m_main_display;
 #endif
 
-    display (const sc_core::sc_module_name& name, QemuDevice& gpu);
+    display(const sc_core::sc_module_name& name, QemuDevice& gpu);
 
     bool is_vnc_enabled()
     {
