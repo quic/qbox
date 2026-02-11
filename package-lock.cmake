f930fd51d9340fddc615c713efd7c97dec6e3fc1
Mahmoud Kamel <mkamel@qti.qualcomm.com>
2026-02-11 12:06:14 +0100

    Package-lock: update libqemu to libqemu-v10.1-v0.10
    - hvf: set the pa bit numbers to maximum supported by the host
    - nvme: disable the reentrancy guard for iomem
    
    Signed-off-by: Mahmoud Kamel <mkamel@qti.qualcomm.com>

--- package-lock.cmake
+++ package-lock.cmake
@@ -27,7 +27,7 @@ CPMDeclarePackage(SCP
 CPMDeclarePackage(qemu
     NAME libqemu
     GIT_REPOSITORY ${GREENSOCS_GIT}${QEMU_PATH_NAME}.git
-    GIT_TAG libqemu-v10.1-v0.9
+    GIT_TAG libqemu-v10.1-v0.10
     GIT_SUBMODULES CMakeLists.txt
     GIT_SHALLOW on
 )
