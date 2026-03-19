d879e26cd4f0427d98c1ae420ba57f33065792d3
Mahmoud Kamel <mkamel@qti.qualcomm.com>
2026-03-19 17:53:54 +0100

    qemu-components: add reset signals to the riscv64 core
    Signed-off-by: Mahmoud Kamel <mkamel@qti.qualcomm.com>

--- qemu-components/cpu_riscv/cpu_riscv64/include/riscv64.h
+++ qemu-components/cpu_riscv/cpu_riscv64/include/riscv64.h
@@ -20,6 +20,9 @@
 
 class QemuCpuRiscv64 : public QemuCpu
 {
+public:
+    sc_core::sc_vector<QemuTargetSignalSocket> irq_in;
+
 protected:
     uint64_t m_hartid;
     gs::async_event m_irq_ev;
@@ -40,8 +43,12 @@ public:
          * non-trivial. It means that the SystemC kernel will never starve...
          */
         , m_irq_ev(true)
+        , irq_in("irq_in", 32)
     {
         m_external_ev |= m_irq_ev;
+        for (auto& irq : irq_in) {
+            m_external_ev |= irq->default_event();
+        }
     }
 
     void before_end_of_elaboration()
@@ -51,6 +58,20 @@ public:
         qemu::CpuRiscv64 cpu(get_qemu_dev());
         cpu.set_prop_int("hartid", m_hartid);
         cpu.set_mip_update_callback(std::bind(&QemuCpuRiscv64::mip_update_cb, this, std::placeholders::_1));
+    }
+
+    void end_of_elaboration() override
+    {
+        QemuCpu::end_of_elaboration();
+
+        // Initialize IRQ sockets with GPIO pin numbers 0-31
+        for (int i = 0; i < 32; i++) {
+            irq_in[i].init(m_dev, i);
+        }
+
+        // Register reset handler - needed for proper reset behavior when system reset is requested
+        qemu::CpuRiscv32 cpu(get_qemu_dev());
+        cpu.register_reset();
     }
 };
 
